519f286edad0d1990429d91207eb97ee912f823a
Daniel Blomdahl <blomdahl.daniel@gmail.com>
2015-06-15 20:16:48 +0200

    Update header.blade.php

--- resources/views/backend/includes/header.blade.php
+++ resources/views/backend/includes/header.blade.php
@@ -110,14 +110,14 @@
                       <!-- The user image in the navbar-->
                       <img src="{!!asset('img/backend/user2-160x160.jpg')!!}" class="user-image" alt="User Image"/>
                       <!-- hidden-xs hides the username on small devices so only the image appears. -->
-                      <span class="hidden-xs">{{ Auth::user()->name }}</span>
+                      <span class="hidden-xs">{{ access()->user()->name }}</span>
                     </a>
                     <ul class="dropdown-menu">
                       <!-- The user image in the menu -->
                       <li class="user-header">
                         <img src="{!!asset('img/backend/user2-160x160.jpg')!!}" class="img-circle" alt="User Image" />
                         <p>
-                          {{ Auth::user()->name }} - Web Developer
+                          {{ access()->user()->name }} - Web Developer
                           <small>Member since XX/XX/XXXX</small>
                         </p>
                       </li>
