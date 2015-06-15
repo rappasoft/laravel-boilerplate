eaaed9a220e207c33f391eb47e704e04f2f4d2ad
Daniel Blomdahl <blomdahl.daniel@gmail.com>
2015-06-15 20:16:09 +0200

    Update sidebar.blade.php

--- resources/views/backend/includes/sidebar.blade.php
+++ resources/views/backend/includes/sidebar.blade.php
@@ -10,7 +10,7 @@
                   <img src="{!!asset('img/backend/user2-160x160.jpg')!!}" class="img-circle" alt="User Image" />
                 </div>
                 <div class="pull-left info">
-                  <p>{{ Auth::user()->name }}</p>
+                  <p>{{ access()->user()->name }}</p>
                   <!-- Status -->
                   <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
                 </div>
