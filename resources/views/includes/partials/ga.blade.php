54f6d2e05f0da61baf7aae99c209e23b9895e23f
Daniel Blomdahl <blomdahl.daniel@gmail.com>
2015-07-14 02:40:35 +0200

    rolled back mistake in my .env (wrong branch)

--- resources/views/includes/partials/ga.blade.php
+++ resources/views/includes/partials/ga.blade.php
@@ -5,5 +5,5 @@
             e=o.createElement(i);r=o.getElementsByTagName(i)[0];
             e.src='//www.google-analytics.com/analytics.js';
             r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','{{ env( 'GOOGLE_ANALYTICS_ID' )}}','auto');ga('send','pageview');
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
         </script>
