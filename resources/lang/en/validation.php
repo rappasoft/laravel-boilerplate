73e9d6217a8391acb72c6ebee6affea6f25833d2
Anthony Rappa <rappa819@gmail.com>
2015-08-27 10:32:49 -0400

    Merge up to larval commit: f40e72fa2fcbe77825d72849f2be59e8e38710aa

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -3,15 +3,15 @@
 return [
 
 	/*
-    |--------------------------------------------------------------------------
-    | Validation Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines contain the default error messages used by
-    | the validator class. Some of these rules have multiple versions such
-    | as the size rules. Feel free to tweak each of these messages here.
-    |
-    */
+	|--------------------------------------------------------------------------
+	| Validation Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines contain the default error messages used by
+	| the validator class. Some of these rules have multiple versions such
+	| as the size rules. Feel free to tweak each of these messages here.
+	|
+	*/
 
 	'accepted'             => 'The :attribute must be accepted.',
 	'active_url'           => 'The :attribute is not a valid URL.',
@@ -41,6 +41,7 @@ return [
 	'in'                   => 'The selected :attribute is invalid.',
 	'integer'              => 'The :attribute must be an integer.',
 	'ip'                   => 'The :attribute must be a valid IP address.',
+	'json'                 => 'The :attribute must be a valid JSON string.',
 	'max'                  => [
 		'numeric' => 'The :attribute may not be greater than :max.',
 		'file'    => 'The :attribute may not be greater than :max kilobytes.',
@@ -76,15 +77,15 @@ return [
 	'url'                  => 'The :attribute format is invalid.',
 
 	/*
-    |--------------------------------------------------------------------------
-    | Custom Validation Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify custom validation messages for attributes using the
-    | convention "attribute.rule" to name the lines. This makes it quick to
-    | specify a specific custom language line for a given attribute rule.
-    |
-    */
+	|--------------------------------------------------------------------------
+	| Custom Validation Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| Here you may specify custom validation messages for attributes using the
+	| convention "attribute.rule" to name the lines. This makes it quick to
+	| specify a specific custom language line for a given attribute rule.
+	|
+	*/
 
 	'custom' => [
 		'attribute-name' => [
@@ -93,15 +94,15 @@ return [
 	],
 
 	/*
-    |--------------------------------------------------------------------------
-    | Custom Validation Attributes
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used to swap attribute place-holders
-    | with something more reader friendly such as E-Mail Address instead
-    | of "email". This simply helps us make messages a little cleaner.
-    |
-    */
+	|--------------------------------------------------------------------------
+	| Custom Validation Attributes
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used to swap attribute place-holders
+	| with something more reader friendly such as E-Mail Address instead
+	| of "email". This simply helps us make messages a little cleaner.
+	|
+	*/
 
 	'attributes' => [],
 
