555806ed07220a0f0d372210c654457d783c579c
Anthony Rappa <rappa819@gmail.com>
2015-09-02 15:06:28 -0400

    Added files for laravels authorization/gate classes but didn't implement them. Updated route files to route global helper methods. Updated to laravel commit: 716e65268ae088e5bd73e505acf9695c127aff66

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,9 +3,10 @@
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+//use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 abstract class Controller extends BaseController {
 
-	use DispatchesJobs, ValidatesRequests;
+	use  DispatchesJobs, ValidatesRequests;
 
 }
--- app/Http/Routes/Backend/Access.php
+++ app/Http/Routes/Backend/Access.php
@@ -3,31 +3,31 @@
 Route::group(['prefix' => 'access', 'namespace' => 'Access'], function ()
 {
 	/* User Management */
-	Route::resource('users', 'UserController', ['except' => ['show']]);
+	resource('users', 'UserController', ['except' => ['show']]);
 
-	Route::get('users/deactivated', ['as' => 'admin.access.users.deactivated', 'uses' => 'UserController@deactivated']);
-	Route::get('users/banned', ['as' => 'admin.access.users.banned', 'uses' => 'UserController@banned']);
-	Route::get('users/deleted', ['as' => 'admin.access.users.deleted', 'uses' => 'UserController@deleted']);
-	Route::get('account/confirm/resend/{user_id}', ['as' => 'admin.account.confirm.resend', 'uses' => 'UserController@resendConfirmationEmail']);
+	get('users/deactivated', ['as' => 'admin.access.users.deactivated', 'uses' => 'UserController@deactivated']);
+	get('users/banned', ['as' => 'admin.access.users.banned', 'uses' => 'UserController@banned']);
+	get('users/deleted', ['as' => 'admin.access.users.deleted', 'uses' => 'UserController@deleted']);
+	get('account/confirm/resend/{user_id}', ['as' => 'admin.account.confirm.resend', 'uses' => 'UserController@resendConfirmationEmail']);
 
 	/* Specific User */
 	Route::group(['prefix' => 'user/{id}', 'where' => ['id' => '[0-9]+']], function () {
-		Route::get('delete', ['as' => 'admin.access.user.delete-permanently', 'uses' => 'UserController@delete']);
-		Route::get('restore', ['as' => 'admin.access.user.restore', 'uses' => 'UserController@restore']);
-		Route::get('mark/{status}', ['as' => 'admin.access.user.mark', 'uses' => 'UserController@mark'])
+		get('delete', ['as' => 'admin.access.user.delete-permanently', 'uses' => 'UserController@delete']);
+		get('restore', ['as' => 'admin.access.user.restore', 'uses' => 'UserController@restore']);
+		get('mark/{status}', ['as' => 'admin.access.user.mark', 'uses' => 'UserController@mark'])
 			->where([
 				'status' => '[0,1,2]'
 			]);
-		Route::get('password/change', ['as' => 'admin.access.user.change-password', 'uses' => 'UserController@changePassword']);
-		Route::post('password/change', ['as' => 'admin.access.user.change-password', 'uses' => 'UserController@updatePassword']);
+		get('password/change', ['as' => 'admin.access.user.change-password', 'uses' => 'UserController@changePassword']);
+		post('password/change', ['as' => 'admin.access.user.change-password', 'uses' => 'UserController@updatePassword']);
 	});
 
 	/* Roles Management */
-	Route::resource('roles', 'RoleController', ['except' => ['show']]);
+	resource('roles', 'RoleController', ['except' => ['show']]);
 
 	/* Permission Management */
 	Route::group(['prefix' => 'roles'], function ()
 	{
-		Route::resource('permissions', 'PermissionController', ['except' => ['show']]);
+		resource('permissions', 'PermissionController', ['except' => ['show']]);
 	});
 });
