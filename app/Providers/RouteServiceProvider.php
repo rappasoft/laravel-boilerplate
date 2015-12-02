c0c1e9745fb5ab7542c8e6da15a4d01d9cb0749b
Andres <andres@empowrd.com>
2015-12-02 11:59:18 -0800

    updated with L5-boiler for cleaner routes handling

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -1,6 +1,6 @@
 <?php namespace App\Providers;
 
-use Illuminate\Routing\Router;
+use Illuminate\Routing\Route;
 use Illuminate\Support\Facades\View;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 
