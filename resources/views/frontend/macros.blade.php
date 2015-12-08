1337d358eacaf70bb48a67798d273873d74a6d53
Anthony Rappa <rappa819@gmail.com>
2015-12-08 12:24:42 -0500

    Added 8 new macro dropdowns and refactored a little bit to make it easier to extend.

--- resources/views/frontend/macros.blade.php
+++ resources/views/frontend/macros.blade.php
@@ -10,12 +10,46 @@
 
                 <div class="panel-body">
                     <div class="form-group">
-                        <label>{{ trans('labels.state') }}</label>
-                        {!! Form::selectState('state', 'NY', ['class' => 'form-control']) !!}
+                        <label>{{ trans('labels.state.us') }}</label>
+                        {{-- Shorthand for this is just selectState, set which version is shorthanded in Macros/Dropdowns --}}
+                        {!! Form::selectStateUS('state', 'NY', ['class' => 'form-control']) !!}
                     </div>
                     <div class="form-group">
-                        <label>{{ trans('labels.country') }}</label>
-                        {!! Form::selectCountry('country', 'US', ['class' => 'form-control']) !!}
+                        <label>{{ trans('labels.state.us.outlying') }}</label>
+                        {!! Form::selectStateUSOutlyingTerritories('state_outlying', null, ['class' => 'form-control']) !!}
+                    </div>
+                    <div class="form-group">
+                        <label>{{ trans('labels.state.us.armed') }}</label>
+                        {!! Form::selectStateUSArmedForces('armed_forces', null, ['class' => 'form-control']) !!}
+                    </div>
+                    <div class="form-group">
+                        <label>{{ trans('labels.territories.canada') }}</label>
+                        {!! Form::selectCanadaTerritories('canada_territories', null, ['class' => 'form-control']) !!}
+                    </div>
+                    <div class="form-group">
+                        <label>{{ trans('labels.state.mexico') }}</label>
+                        {!! Form::selectStateMexico('mexico', null, ['class' => 'form-control']) !!}
+                    </div>
+                    <div class="form-group">
+                        <label>{{ trans('labels.country.alpha') }}</label>
+                        {!! Form::selectCountryAlpha('country_alpha', 'ISO 3166-2:US', ['class' => 'form-control']) !!}
+                    </div>
+                    <div class="form-group">
+                        <label>{{ trans('labels.country.alpha2') }}</label>
+                        {{-- Shorthand for this is just selectCountry, set which version is shorthanded in Macros/Dropdowns --}}
+                        {!! Form::selectCountryAlpha2('country_alpha2', 'US', ['class' => 'form-control']) !!}
+                    </div>
+                    <div class="form-group">
+                        <label>{{ trans('labels.country.alpha3') }}</label>
+                        {!! Form::selectCountryAlpha3('country_alpha3', 'USA', ['class' => 'form-control']) !!}
+                    </div>
+                    <div class="form-group">
+                        <label>{{ trans('labels.country.numeric') }}</label>
+                        {!! Form::selectCountryNumeric('country_numeric', '840', ['class' => 'form-control']) !!}
+                    </div>
+                    <div class="form-group">
+                        <label>{{ trans('labels.timezone') }}</label>
+                        {!! Form::selectTimezone('timezone', 'America/New_York', ['class' => 'form-control']) !!}
                     </div>
                 </div>
             </div><!-- panel -->
