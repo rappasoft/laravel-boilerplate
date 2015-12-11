3eb74e578e2e57dd585bc50a9d437056f80bb7fc
Helmer Davila <helmer290192@hotmail.com>
2015-12-11 14:15:19 -0500

    All tests folder to PSR-2 Standards

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,36 +1,39 @@
 <?php
 
-require_once __DIR__.'/../bootstrap/autoload.php';
+require_once __DIR__ . '/../bootstrap/autoload.php';
 
 /**
  * Class TestCase
  */
-class TestCase extends Illuminate\Foundation\Testing\TestCase {
-	/**
-	 * The base URL to use while testing the application.
-	 *
-	 * @var string
-	 */
-	protected $baseUrl;
+class TestCase extends Illuminate\Foundation\Testing\TestCase
+{
+    /**
+     * The base URL to use while testing the application.
+     *
+     * @var string
+     */
+    protected $baseUrl;
 
-	/**
-	 *
-	 */
-	public function __construct() {
-		Dotenv::load(__DIR__.'/..');
-		$this->baseUrl = Dotenv::findEnvironmentVariable('APP_URL');
-	}
+    /**
+     *
+     */
+    public function __construct()
+    {
+        Dotenv::load(__DIR__ . '/..');
+        $this->baseUrl = Dotenv::findEnvironmentVariable('APP_URL');
+    }
 
-	/**
-	 * Creates the application.
-	 *
-	 * @return \Illuminate\Foundation\Application
-	 */
-	public function createApplication() {
-		$app = require __DIR__ . '/../bootstrap/app.php';
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__ . '/../bootstrap/app.php';
 
-		$app->make( Illuminate\Contracts\Console\Kernel::class )->bootstrap();
+        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
-		return $app;
-	}
+        return $app;
+    }
 }
--- tests/frontend/AccessTest.php
+++ tests/frontend/AccessTest.php
@@ -5,69 +5,74 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 /**
  * Class AccessTest
  */
-class AccessTest extends TestCase {
+class AccessTest extends TestCase
+{
+    use DatabaseTransactions;
 
-	use DatabaseTransactions;
+    /**
+     *
+     */
+    public function test_user_can_see_auth_register()
+    {
+        $this->visit('/auth/register')->see('Register');
+    }
 
-	/**
-	 *
-	 */
-	public function test_user_can_see_auth_register(){
-		$this->visit( '/auth/register' )->see('Register');
-	}
+    /**
+     *
+     */
+    public function test_user_can_see_auth_login()
+    {
+        $this->visit('/auth/login')->see('Login');
+    }
 
-	/**
-	 *
-	 */
-	public function test_user_can_see_auth_login(){
-		$this->visit( '/auth/login' )->see('Login');
-	}
+    /**
+     *
+     */
+    public function test_redirect_to_login_if_i_try_to_view_dashboard_without_logging_in()
+    {
+        $this->visit('/dashboard')
+            ->seePageIs('/auth/login')
+            ->see('Login');
+    }
 
-	/**
-	 *
-	 */
-	public function test_redirect_to_login_if_i_try_to_view_dashboard_without_logging_in() {
-		$this->visit( '/dashboard' )
-			->seePageIs( '/auth/login' )
-			->see( 'Login' );
-	}
+    /**
+     * TODO: Fix errors on swiftmailer
+     */
+    /*public function test_can_create_an_account() {
+    $this->visit( '/auth/register' )
+    ->type( 'Admin Istrator', 'name' )
+    ->type( 'test@test.com', 'email' )
+    ->type( '123456', 'password' )
+    ->type( '123456', 'password_confirmation' )
+    ->press( 'Register' )
+    ->seeInDatabase( 'users', [ 'email' => 'test@test.com' ] );
+    }*/
 
-	/**
-	 * TODO: Fix errors on swiftmailer
-	 */
-	/*public function test_can_create_an_account() {
-		$this->visit( '/auth/register' )
-			->type( 'Admin Istrator', 'name' )
-			->type( 'test@test.com', 'email' )
-			->type( '123456', 'password' )
-			->type( '123456', 'password_confirmation' )
-			->press( 'Register' )
-			->seeInDatabase( 'users', [ 'email' => 'test@test.com' ] );
-	}*/
+    /**
+     *
+     */
+    public function test_user_can_login_without_remember_me()
+    {
+        $this->visit('/auth/login')
+            ->type('user@user.com', 'email')
+            ->type('1234', 'password')
+            ->press('Login')
+            ->seePageIs('/dashboard')
+            ->see('Dashboard');
+    }
 
-	/**
-	 *
-	 */
-	public function test_user_can_login_without_remember_me(){
-		$this->visit( '/auth/login' )
-			->type( 'user@user.com', 'email' )
-			->type( '1234', 'password' )
-			->press( 'Login' )
-			->seePageIs( '/dashboard' )
-			->see('Dashboard');
-	}
-
-	/**
-	 *
-	 */
-	public function test_user_can_login_with_remember_me(){
-		$this->visit( '/auth/login' )
-			->type( 'user@user.com', 'email' )
-			->type( '1234', 'password' )
-			->check('remember')
-			->press( 'Login' )
-			->seePageIs( '/dashboard' )
-			//missing to check weather remember-me session/cookie exist
-			->see('Dashboard');
-	}
+    /**
+     *
+     */
+    public function test_user_can_login_with_remember_me()
+    {
+        $this->visit('/auth/login')
+            ->type('user@user.com', 'email')
+            ->type('1234', 'password')
+            ->check('remember')
+            ->press('Login')
+            ->seePageIs('/dashboard')
+        //missing to check weather remember-me session/cookie exist
+            ->see('Dashboard');
+    }
 }
