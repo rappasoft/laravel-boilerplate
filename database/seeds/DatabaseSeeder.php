d24f5cdfd9a869be219758ed23e1b059c1b102bc
Helmer Davila <helmer290192@hotmail.com>
2015-12-11 14:13:35 -0500

    All database folder to PSR-2 Standards

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,27 +1,29 @@
 <?php
 
-use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder {
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		Model::unguard();
+        if (env('DB_DRIVER') == 'mysql') {
+            DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        }
 
-		if(env('DB_DRIVER')=='mysql')
-			DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        $this->call(AccessTableSeeder::class);
 
-		$this->call(AccessTableSeeder::class);
+        if (env('DB_DRIVER') == 'mysql') {
+            DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        }
 
-		if(env('DB_DRIVER')=='mysql')
-			DB::statement('SET FOREIGN_KEY_CHECKS=1;');
-
-		Model::reguard();
-	}
+        Model::reguard();
+    }
 }
