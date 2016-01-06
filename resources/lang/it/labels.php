c1eb447c11fe733828a1ae24842bca9064f46c91
Andrea Bergamasco <info@vjandrea.net>
2016-01-06 22:01:40 +0100

    Italian translation for version 2.0.10

---
+++ resources/lang/it/labels.php
@@ -0,0 +1,184 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Labels Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in labels throughout the system.
+    | Regardless where it is placed, a label can be listed here so it is easily
+    | found in a intuitive way.
+    |
+    */
+
+    'general' => [
+        'all' => 'All',
+        'actions' => 'Actions',
+        'buttons' => [
+            'save' => 'Save',
+            'update' => 'Update',
+        ],
+        'hide' => 'Hide',
+        'none' => 'None',
+        'show' => 'Show',
+        'toggle_navigation' => 'Toggle Navigation',
+    ],
+
+    'backend' => [
+        'access' => [
+            'permissions' => [
+                'create' => 'Create Permission',
+                'dependencies' => 'Dependencies',
+                'edit' => 'Edit Permission',
+
+                'groups' => [
+                    'create' => 'Create Group',
+                    'edit' => 'Edit Group',
+
+                    'table' => [
+                        'name' => 'Name',
+                    ],
+                ],
+
+                'grouped_permissions' => 'Grouped Permissions',
+                'label' => 'permissions',
+                'management' => 'Permission Management',
+                'no_groups' => 'There are no permission groups.',
+                'no_permissions' => 'No permission to choose from.',
+                'no_roles' => 'No Roles to set',
+                'no_ungrouped' => 'There are no ungrouped permissions.',
+
+                'table' => [
+                    'dependencies' => 'Dependencies',
+                    'group' => 'Group',
+                    'group-sort' => 'Group Sort',
+                    'name' => 'Name',
+                    'permission' => 'Permission',
+                    'roles' => 'Roles',
+                    'system' => 'System',
+                    'total' => 'permissions(s) total',
+                    'users' => 'Users',
+                ],
+
+                'tabs' => [
+                    'general' => 'General',
+                    'groups' => 'Groups',
+                    'dependencies' => 'Dependencies',
+                    'permissions' => 'Permissions',
+                ],
+
+                'ungrouped_permissions' => 'Ungrouped Permissions',
+            ],
+
+            'roles' => [
+                'create' => 'Create Role',
+                'edit' => 'Edit Role',
+                'management' => 'Role Management',
+
+                'table' => [
+                    'number_of_users' => 'Number of Users',
+                    'permissions' => 'Permissions',
+                    'role' => 'Role',
+                    'sort' => 'Sort',
+                    'total' => 'roles(s) total',
+                ],
+            ],
+
+            'users' => [
+                'active' => 'Active Users',
+                'all_permissions' => 'All Permissions',
+                'change_password' => 'Change Password',
+                'change_password_for' => 'Change Password for :user',
+                'create' => 'Create User',
+                'deactivated' => 'Deactivated Users',
+                'deleted' => 'Deleted Users',
+                'dependencies' => 'Dependencies',
+                'edit' => 'Edit User',
+                'management' => 'User Management',
+                'no_other_permissions' => 'No Other Permissions',
+                'no_permissions' => 'No Permissions',
+                'no_roles' => 'No Roles to set.',
+                'permissions' => 'Permissions',
+                'permission_check' => 'Checking a permission will also check its dependencies, if any.',
+
+                'table' => [
+                    'confirmed' => 'Confirmed',
+                    'created' => 'Created',
+                    'email' => 'E-mail',
+                    'id' => 'ID',
+                    'last_updated' => 'Last Updated',
+                    'name' => 'Name',
+                    'no_deactivated' => 'No Deactivated Users',
+                    'no_deleted' => 'No Deleted Users',
+                    'other_permissions' => 'Other Permissions',
+                    'roles' => 'Roles',
+                    'total' => 'user(s) total',
+                ],
+            ],
+        ],
+    ],
+
+    'frontend' => [
+
+        'auth' => [
+            'login_box_title' => 'Login',
+            'login_button' => 'Login',
+            'login_with' => 'Login with :social_media',
+            'register_box_title' => 'Register',
+            'register_button' => 'Register',
+            'remember_me' => 'Remember Me',
+        ],
+
+        'passwords' => [
+            'forgot_password' => 'Forgot Your Password?',
+            'reset_password_box_title' => 'Reset Password',
+            'reset_password_button' => 'Reset Password',
+            'send_password_reset_link_button' => 'Send Password Reset Link',
+        ],
+
+        'macros' => [
+            'country' => [
+                'alpha' => 'Country Alpha Codes',
+                'alpha2' => 'Country Alpha 2 Codes',
+                'alpha3' => 'Country Alpha 3 Codes',
+                'numeric' => 'Country Numeric Codes',
+            ],
+
+            'macro_examples' => 'Macro Examples',
+
+            'state' => [
+                'mexico' => 'Mexico State List',
+                'us' => [
+                    'us' => 'US States',
+                    'outlying' => 'US Outlying Territories',
+                    'armed' => 'US Armed Forces',
+                ],
+            ],
+
+            'territories' => [
+                'canada' => 'Canada Province & Territories List',
+            ],
+
+            'timezone' => 'Timezone',
+        ],
+
+        'user' => [
+            'passwords' => [
+                'change' => 'Change Password',
+            ],
+
+            'profile' => [
+                'avatar' => 'Avatar',
+                'created_at' => 'Created At',
+                'edit_information' => 'Edit Information',
+                'email' => 'E-mail',
+                'last_updated' => 'Last Updated',
+                'name' => 'Name',
+                'update_information' => 'Update Information',
+            ],
+        ],
+
+    ],
+];
---
+++ resources/lang/it/navs.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Navs Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in menu items throughout the system.
+    | Regardless where it is placed, a menu item can be listed here so it is easily
+    | found in a intuitive way.
+    |
+    */
+
+    'backend' => [
+        'button' => 'Button',
+    ],
+
+    'general' => [
+        'logout' => 'Logout',
+    ],
+
+    'frontend' => [
+        'dashboard' => 'Dashboard',
+        'home' => 'Home',
+        'login' => 'Login',
+        'macros' => 'Macros',
+        'register' => 'Register',
+
+        'user' => [
+            'administration' => 'Administration',
+            'change_password' => 'Change Password',
+            'my_information' => 'My Information',
+        ],
+    ],
+];
