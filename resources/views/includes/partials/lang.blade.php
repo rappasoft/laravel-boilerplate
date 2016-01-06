20273b0524de06f0b9f89d93b3ab2327c4cb1638
blomdahldaniel <blomdahl.daniel@gmail.com>
2016-01-06 16:33:56 +0100

    dropdown list item

--- resources/views/includes/partials/lang.blade.php
+++ resources/views/includes/partials/lang.blade.php
@@ -1,4 +1,5 @@
 <ul class="dropdown-menu" role="menu">
     <li>{!! link_to('lang/en', trans('menus.language-picker.langs.en')) !!}</li>
     <li>{!! link_to('lang/fr-FR', trans('menus.language-picker.langs.fr-FR')) !!}</li>
+    <li>{!! link_to('lang/sv', trans('menus.language-picker.langs.sv')) !!}</li>
 </ul>
