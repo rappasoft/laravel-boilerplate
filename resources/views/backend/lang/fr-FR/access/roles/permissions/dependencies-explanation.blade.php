609f25c896da4ca5eefc23c5526712e944d68f94
error500 <error500@free.fr>
2016-01-07 08:55:46 +0100

    pack fr convert to UTF8-without BOM and some fix

--- resources/views/backend/lang/fr-FR/access/roles/permissions/dependencies-explanation.blade.php
+++ resources/views/backend/lang/fr-FR/access/roles/permissions/dependencies-explanation.blade.php
@@ -1,2 +1,2 @@
-Cette section vous permet de spécifier que les dépendances de la permission.<br/><br/>
-    Par exemple : Si la permission est  <strong>create-user</strong>, cochez les permissions <strong>view-backend</strong> et <strong>view-access-management</strong>, pour que ce dernier soit en mesure d'accéder à l'écran de <strong>Création d'un utilisaeut</strong>.
+Cette section vous permet de spécifier les dépendances de la permission.<br/><br/>
+    Par exemple : Si la permission est  <strong>create-user</strong>, cochez les permissions <strong>view-backend</strong> et <strong>view-access-management</strong>, pour que cette permission donne accès à l'écran de <strong>Création d'un utilisaeut</strong>.
