d6e3aa87d9f2450883006d2f79a2c3be49ee8d05
Andrea Bergamasco <abergamasco@gmail.com>
2016-01-10 19:54:31 +0100

    Italian translation - Middleware and backend views

---
+++ resources/views/backend/lang/it/access/roles/associated-permissions-explanation.blade.php
@@ -0,0 +1,2 @@
+Un permesso che ha a fianco del nome una <small><strong>(D)</strong></small> indica che ci sono delle dipendenze che verranno controllate automaticamente quando selezionerai quel permesso.
+Potrai gestire ogni singola dipendenza nella scheda <strong>Dipedenze</strong> della pagina <strong>Modifica permessi</strong>.
