3f352b1c8a4504f040b6457f48c8292a075d4941
Anthony Rappa <rappa819@gmail.com>
2016-01-27 10:42:50 -0500

    Added token mismatch catch for when sessions expire instead of whoops error. Replaced deprecated use call in exception handler. Updated laravel version.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,10 +3,11 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Session\TokenMismatchException;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
-use Illuminate\Foundation\Validation\ValidationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 /**
@@ -49,6 +50,14 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
+        /**
+         * Redirect if token mismatch error
+         * Usually because user stayed on the same screen too long and their session expired
+         */
+        if ($e instanceof TokenMismatchException) {
+            return redirect('/login');
+        }
+
         /**
          * All instances of GeneralException redirect back with a flash message to show a bootstrap alert-error
          */
