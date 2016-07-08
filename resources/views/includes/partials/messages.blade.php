2e7e699c375a967923f9030738ab3a1bcf2095a6
Anthony Rappa <rappa819@gmail.com>
2016-07-07 23:53:53 -0400

    Fixed error messages not parsing html links.

--- resources/views/includes/partials/messages.blade.php
+++ resources/views/includes/partials/messages.blade.php
@@ -1,47 +1,47 @@
 @if ($errors->any())
     <div class="alert alert-danger">
         @foreach ($errors->all() as $error)
-            {{ $error }}<br/>
+            {!! $error !!}<br/>
         @endforeach
     </div>
 @elseif (session()->get('flash_success'))
     <div class="alert alert-success">
         @if(is_array(json_decode(session()->get('flash_success'), true)))
-            {{ implode('', session()->get('flash_success')->all(':message<br/>')) }}
+            {!! implode('', session()->get('flash_success')->all(':message<br/>')) !!}
         @else
-            {{ session()->get('flash_success') }}
+            {!! session()->get('flash_success') !!}
         @endif
     </div>
 @elseif (session()->get('flash_warning'))
     <div class="alert alert-warning">
         @if(is_array(json_decode(session()->get('flash_warning'), true)))
-            {{ implode('', session()->get('flash_warning')->all(':message<br/>')) }}
+            {!! implode('', session()->get('flash_warning')->all(':message<br/>')) !!}
         @else
-            {{ session()->get('flash_warning') }}
+            {!! session()->get('flash_warning') !!}
         @endif
     </div>
 @elseif (session()->get('flash_info'))
     <div class="alert alert-info">
         @if(is_array(json_decode(session()->get('flash_info'), true)))
-            {{ implode('', session()->get('flash_info')->all(':message<br/>')) }}
+            {!! implode('', session()->get('flash_info')->all(':message<br/>')) !!}
         @else
-            {{ session()->get('flash_info') }}
+            {!! session()->get('flash_info') !!}
         @endif
     </div>
 @elseif (session()->get('flash_danger'))
     <div class="alert alert-danger">
         @if(is_array(json_decode(session()->get('flash_danger'), true)))
-            {{ implode('', session()->get('flash_danger')->all(':message<br/>')) }}
+            {!! implode('', session()->get('flash_danger')->all(':message<br/>')) !!}
         @else
-            {{ session()->get('flash_danger') }}
+            {!! session()->get('flash_danger') !!}
         @endif
     </div>
 @elseif (session()->get('flash_message'))
     <div class="alert alert-info">
         @if(is_array(json_decode(session()->get('flash_message'), true)))
-            {{ implode('', session()->get('flash_message')->all(':message<br/>')) }}
+            {!! implode('', session()->get('flash_message')->all(':message<br/>')) !!}
         @else
-            {{ session()->get('flash_message') }}
+            {!! session()->get('flash_message') !!}
         @endif
     </div>
 @endif
