e058eaeae4f9302e50d878f526df1d925157c7e6
Anthony Rappa <rappa819@gmail.com>
2016-08-23 10:35:24 -0400

    Added line to force SSL in production, commented out by default.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -39,6 +40,11 @@ class AppServiceProvider extends ServiceProvider
         } else {
             session()->forget('lang-rtl');
         }
+
+        //Force SSL in production
+		if ($this->app->environment() == 'production') {
+			//URL::forceSchema('https');
+		}
     }
 
     /**
