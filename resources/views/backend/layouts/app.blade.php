df46af6f21abc7470752fbc9b74108fa19a95be3
Anthony Rappa <rappa819@gmail.com>
2016-10-09 16:50:58 -0400

    Fixed facade calls.

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -27,11 +27,11 @@
 
         @yield('after-styles-end')
 
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- Html5 Shim and Respond.js IE8 support of Html5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
         <!--[if lt IE 9]>
-        {{ HTML::script('https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js') }}
-        {{ HTML::script('https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js') }}
+        {{ Html::script('https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js') }}
+        {{ Html::script('https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js') }}
         <![endif]-->
 
         <!-- Scripts -->
@@ -70,7 +70,7 @@
 
         <!-- JavaScripts -->
         @yield('before-scripts-end')
-        {{ HTML::script(elixir('js/backend.js')) }}
+        {{ Html::script(elixir('js/backend.js')) }}
         @yield('after-scripts-end')
     </body>
 </html>
