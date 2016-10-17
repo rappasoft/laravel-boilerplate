903575975c65746180a916d8e6ce59600874a4cf
Anthony Rappa <rappa819@gmail.com>
2016-10-17 14:27:24 -0400

    - Manually add PR #503

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+		'address' => env('MAIL_FROM','hello@example.com'),
+		'name' => env('MAIL_FROM_NAME','Example'),
     ],
 
     /*
