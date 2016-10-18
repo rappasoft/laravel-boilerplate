1903ee2bfd1b436b3092c9265e5977787f0f26d5
poseso <carlosmsanchez@gmail.com>
2016-10-18 12:48:15 -0400

    Update lang.blade.php Hide language
    Hide current used language from the language dropdown menu, if using a language it doesnt make any sense to still see it if its currently in use.

--- resources/views/includes/partials/lang.blade.php
+++ resources/views/includes/partials/lang.blade.php
@@ -1,5 +1,7 @@
 <ul class="dropdown-menu" role="menu">
     @foreach (array_keys(config('locale.languages')) as $lang)
-        <li>{{ link_to('lang/'.$lang, trans('menus.language-picker.langs.'.$lang)) }}</li>
+        @if ($lang != App::getLocale())
+            <li>{{ link_to('lang/'.$lang, trans('menus.language-picker.langs.'.$lang)) }}</li>
+        @endif
     @endforeach
 </ul>
