529b277c339e78fc01debafd03c08f6950b5c83b
blomdahldaniel <blomdahl.daniel@gmail.com>
2016-10-24 11:03:53 +0200

    "de" lang for confirm and reset password emails

--- resources/lang/de/strings.php
+++ resources/lang/de/strings.php
@@ -58,16 +58,22 @@ return [
 
     'emails' => [
         'auth' => [
+            'error' => 'Whoops!',
+            'greeting' => 'Hello!',
+            'regards' => 'Regards,',
+            'trouble_clicking_button' => 'If you’re having trouble clicking the ":action_text" button, copy and paste the URL below into your web browser:',
+            'thank_you_for_using_app' => 'Thank you for using our application!',
+
             'password_reset_subject' => 'Dein Link zum zurücksetzen des Passworts',
+            'password_cause_of_email' => 'You are receiving this email because we received a password reset request for your account.',
+            'password_if_not_requested' => 'If you did not request a password reset, no further action is required.',
             'reset_password' => 'klick hier um dein Passwort zurückzusetzen',
+
+            'click_to_confirm' => 'Klick hier um deinen Account zu aktivieren:',
         ],
     ],
 
     'frontend' => [
-        'email' => [
-            'confirm_account' => 'Klick hier um deinen Account zu aktivieren:',
-        ],
-
         'test' => 'Test',
 
         'tests' => [
