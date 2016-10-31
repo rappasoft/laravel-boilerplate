633bf18cb1dba15fc61e033364b4b1b908046eb2
Anthony Rappa <rappa819@gmail.com>
2016-10-31 12:23:39 -0400

    - If confirm email is on check to see if the notification was sent in the test

--- tests/unit/frontend/FormTest.php
+++ tests/unit/frontend/FormTest.php
@@ -1,7 +1,10 @@
 <?php
 
 use Faker\Factory;
+use App\Models\Access\User\User;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Notifications\Frontend\Auth\UserNeedsConfirmation;
 
 /**
  * Class FormTest
@@ -10,12 +13,21 @@ class FormTest extends TestCase
 {
 	use DatabaseTransactions;
 
+	/**
+	 * Test the registration form
+	 * Test it works with confirming email on or off, and that the confirm email notification is sent
+	 */
 	public function testRegistrationForm() {
+		// Note: Captcha is disabled by default in phpunit.xml
+
+		// Create any needed resources
 		$faker = Faker\Factory::create();
 		$password = $faker->password(8);
 
 		// Check if confirmation required is on or off
-		if (config('access.users.confirm_email') === true) {
+		if (config('access.users.confirm_email')) {
+			Notification::fake();
+
 			$this->visit('/register')
 				->type($faker->name, 'name')
 				->type($faker->safeEmail, 'email')
@@ -25,6 +37,14 @@ class FormTest extends TestCase
 				->see('Your account was successfully created. We have sent you an e-mail to confirm your account.')
 				->see('Login')
 				->seePageIs('/');
+
+			// Get the last user inserted into the database
+			$user = User::orderBy('created_at', 'desc')->first();
+
+			// Check that the user was sent the confirmation email
+			Notification::assertSentTo(
+				[$user], UserNeedsConfirmation::class
+			);
 		} else {
 			$this->visit('/register')
 				->type($faker->name, 'name')
