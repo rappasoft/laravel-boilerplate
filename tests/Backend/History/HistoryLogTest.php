434b59ce3938530fb242c70d644d18d400373cd3
Anthony Rappa <rappa819@gmail.com>
2016-11-15 15:06:32 -0500

    - Made sure history log function can take a type by its ID, added test as well.

--- tests/Backend/History/HistoryLogTest.php
+++ tests/Backend/History/HistoryLogTest.php
@@ -7,7 +7,7 @@ use App\Repositories\Backend\History\Facades\History;
  */
 class HistoryLogTest extends TestCase
 {
-	public function testHistoryLogFunction() {
+	public function testHistoryLogByTypeNameFunction() {
 		$this->actingAs($this->admin);
 
 		History::log('User', 'trans("history.backend.users.created") ' . $this->user->name, $this->user->id, 'plus', 'bg-green');
@@ -22,5 +22,22 @@ class HistoryLogTest extends TestCase
 		])
 		->visit('/admin/dashboard')
 		->see('<strong>'.$this->admin->name.'</strong> created user ' . $this->user->name);
+	}
+
+	public function testHistoryLogByTypeIdFunction() {
+		$this->actingAs($this->admin);
+
+		History::log(1, 'trans("history.backend.users.created") ' . $this->user->name, $this->user->id, 'plus', 'bg-green');
+
+		$this->seeInDatabase('history', [
+			'type_id' => 1,
+			'user_id' => $this->admin->id,
+			'entity_id' => $this->user->id,
+			'icon' => 'plus',
+			'class' => 'bg-green',
+			'text' => 'trans("history.backend.users.created") ' . $this->user->name,
+		])
+			->visit('/admin/dashboard')
+			->see('<strong>'.$this->admin->name.'</strong> created user ' . $this->user->name);
 	}
 }
