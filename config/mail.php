5b53dfd3a9e65c97e7b94a60eb57f09b2cbcce57
Valentin Ivaşcu <oriceon@gmail.com>
2016-11-18 13:11:10 +0200

    Allow self signed SSL certifficates

--- config/mail.php
+++ config/mail.php
@@ -111,5 +111,12 @@ return [
     */
 
     'sendmail' => '/usr/sbin/sendmail -bs',
+	
+    'stream' => [
+        'ssl' => [
+            'allow_self_signed' => true,
+            'verify_peer'       => false,
+        ],
+    ],
 
 ];
