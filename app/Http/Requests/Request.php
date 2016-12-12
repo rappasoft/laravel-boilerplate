333b84daeaf1a0471b9bb170ef83d4e5070b5186
Bryan Scott <bryan.scott.si@gmail.com>
2016-12-13 08:59:42 +1100

    Add default permission declined message when permission is denied in Request

--- app/Http/Requests/Request.php
+++ app/Http/Requests/Request.php
@@ -10,5 +10,16 @@ use Illuminate\Foundation\Http\FormRequest;
  */
 abstract class Request extends FormRequest
 {
-    //
+    // @Acknowledgement
+    // https://www.damchey.com/2015/06/redirect-all-laravel-5-formrequest-authorize-failures-to-a-routeurl/
+    //A generic error message, can be overridden in the sub class
+    protected $error = '';
+
+    public function forbiddenResponse()
+    {
+        if (empty($error)) {
+            $this->error = trans('auth.general_error');
+        }
+        return redirect()->back()->withErrors($this->error);
+    }
 }
