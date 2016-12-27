0da59f097b84118cffeb52f7e7e2bb371cf4e569
Anthony Rappa <rappa819@gmail.com>
2016-12-27 12:00:53 -0500

    no message

--- app/Http/Requests/Request.php
+++ app/Http/Requests/Request.php
@@ -8,18 +8,22 @@ use Illuminate\Foundation\Http\FormRequest;
  * Class Request
  * @package App\Http\Requests
  */
-abstract class Request extends FormRequest
-{
-    // @Acknowledgement
-    // https://www.damchey.com/2015/06/redirect-all-laravel-5-formrequest-authorize-failures-to-a-routeurl/
-    //A generic error message, can be overridden in the sub class
-    protected $error = '';
+abstract class Request extends FormRequest {
 
-    public function forbiddenResponse()
+	/**
+	 * @var string
+	 */
+	protected $error = '';
+
+	/**
+	 * @return $this
+	 */
+	public function forbiddenResponse()
     {
         if (empty($error)) {
             $this->error = trans('auth.general_error');
         }
+
         return redirect()->back()->withErrors($this->error);
     }
 }
