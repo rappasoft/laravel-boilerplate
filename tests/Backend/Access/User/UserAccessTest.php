0ed6c1093e8dbffea7a1bda15219bf6b569edfed
slavkobabic <mail@slavkobabic.com>
2017-01-11 00:18:53 +0100

    Removed extra spaces.

--- tests/Backend/Access/User/UserAccessTest.php
+++ tests/Backend/Access/User/UserAccessTest.php
@@ -6,69 +6,69 @@ use App\Events\Backend\Access\User\UserDeleted;
  */
 class UserAccessTest extends TestCase {
 	public function testUserCantAccessAdminDashboard() {
-		$this->visit( '/' )
+		$this->visit('/')
 		     ->actingAs( $this->user )
-		     ->visit( '/admin/dashboard' )
-		     ->seePageIs( '/' )
-		     ->see( 'You do not have access to do that.' );
+		     ->visit('/admin/dashboard')
+		     ->seePageIs('/')
+		     ->see('You do not have access to do that.');
 	}
 
 	public function testExecutiveCanAccessAdminDashboard() {
-		$this->visit( '/' )
+		$this->visit('/')
 		     ->actingAs( $this->executive )
-		     ->visit( '/admin/dashboard' )
-		     ->seePageIs( '/admin/dashboard' )
+		     ->visit('/admin/dashboard')
+		     ->seePageIs('/admin/dashboard')
 		     ->see( $this->executive->name );
 	}
 
 	public function testExecutiveCantAccessManageRoles() {
-		$this->visit( '/' )
+		$this->visit('/')
 		     ->actingAs( $this->executive )
-		     ->visit( '/admin/dashboard' )
-		     ->seePageIs( '/admin/dashboard' )
-		     ->visit( '/admin/access/role' )
-		     ->seePageIs( '/' )
-		     ->see( 'You do not have access to do that.' );
+		     ->visit('/admin/dashboard')
+		     ->seePageIs('/admin/dashboard')
+		     ->visit('/admin/access/role')
+		     ->seePageIs('/')
+		     ->see('You do not have access to do that.');
 	}
 
 	public function testExecutiveCantAccessEditAdminUser() {
-		$this->visit( '/' )
+		$this->visit('/')
 		     ->actingAs( $this->executive )
-		     ->visit( '/admin/dashboard' )
-		     ->seePageIs( '/admin/dashboard' )
-		     ->visit( '/admin/access/user/1/edit' )
-		     ->seePageIs( '/admin/dashboard' )
-		     ->see( 'You do not have access to do that.' );
+		     ->visit('/admin/dashboard')
+		     ->seePageIs('/admin/dashboard')
+		     ->visit('/admin/access/user/1/edit')
+		     ->seePageIs('/admin/dashboard')
+		     ->see('You do not have access to do that.');
 	}
 
 	public function testExecutiveCantAccessToLoginAsAdministrator() {
-		$this->visit( '/' )
+		$this->visit('/')
 		     ->actingAs( $this->executive )
-		     ->visit( '/admin/access/user' )
-		     ->seePageIs( '/admin/access/user' )
-		     ->visit( '/admin/access/user/1/login-as' )
-		     ->seePageIs( '/admin/access/user' )
-		     ->see( 'You do not have access to do that.' );
+		     ->visit('/admin/access/user')
+		     ->seePageIs('/admin/access/user')
+		     ->visit('/admin/access/user/1/login-as')
+		     ->seePageIs('/admin/access/user')
+		     ->see('You do not have access to do that.');
 	}
 
 	public function testExecutiveCantAccessChangeAdministratorPassword() {
-		$this->visit( '/' )
+		$this->visit('/')
 		     ->actingAs( $this->executive )
-		     ->visit( '/admin/access/user' )
-		     ->seePageIs( '/admin/access/user' )
-		     ->visit( '/admin/access/user/1/password/change' )
-		     ->seePageIs( '/admin/access/user' )
-		     ->see( 'You do not have access to do that.' );
+		     ->visit('/admin/access/user')
+		     ->seePageIs('/admin/access/user')
+		     ->visit('/admin/access/user/1/password/change')
+		     ->seePageIs('/admin/access/user')
+		     ->see('You do not have access to do that.');
 	}
 
 	public function testExecutiveCantAccessChangeAdministratorAccountStatus() {
-		$this->visit( '/' )
+		$this->visit('/')
 		     ->actingAs( $this->executive )
-		     ->visit( '/admin/access/user' )
-		     ->seePageIs( '/admin/access/user' )
-		     ->visit( '/admin/access/user/1/mark/0' )
-		     ->seePageIs( '/admin/access/user' )
-		     ->see( 'You do not have access to do that.' );
+		     ->visit('/admin/access/user')
+		     ->seePageIs('/admin/access/user')
+		     ->visit('/admin/access/user/1/mark/0')
+		     ->seePageIs('/admin/access/user')
+		     ->see('You do not have access to do that.');
 	}
 
 }
