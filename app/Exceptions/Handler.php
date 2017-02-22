f75f45266ba0400c66262d7dbce2b9848fdc3710
Anthony Rappa <rappa819@gmail.com>
2017-02-21 22:58:51 -0500

    - Update to laravel commit: c1643bf0c59b6864fc55e09cce3bfafc67d29e2a

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -76,6 +76,6 @@ class Handler extends ExceptionHandler
             return response()->json(['error' => 'Unauthenticated.'], 401);
         }
 
-        return redirect()->guest('login');
+        return redirect()->guest(route('frontend.auth.login'));
     }
 }
