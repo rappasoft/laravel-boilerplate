b7902d05ff9acf3e6e3a2c9acc435b593381ff13
Anthony Rappa <anthony@rappasoft.com>
2017-02-22 08:05:35 +0000

    Apply fixes from StyleCI

--- routes/Backend/Search.php
+++ routes/Backend/Search.php
@@ -1,11 +1,11 @@
 <?php
 
-Route::group(['namespace' => 'Search'], function() {
-	Route::prefix('search')->as('search.')->group(function () {
+Route::group(['namespace' => 'Search'], function () {
+    Route::prefix('search')->as('search.')->group(function () {
 
-		/*
-		 * Search Specific Functionality
-		 */
-		Route::name('index')->get('/', 'SearchController@index');
-	});
+        /*
+         * Search Specific Functionality
+         */
+        Route::name('index')->get('/', 'SearchController@index');
+    });
 });
--- tests/Backend/Routes/LogViewerRouteTest.php
+++ tests/Backend/Routes/LogViewerRouteTest.php
@@ -10,22 +10,22 @@ class LogViewerRouteTest extends BrowserKitTestCase
     public function testLogViewerDashboard()
     {
         $this->actingAs($this->admin)
-			->visit('/admin/log-viewer')
-			->see('Log Viewer');
+            ->visit('/admin/log-viewer')
+            ->see('Log Viewer');
     }
 
     public function testLogViewerList()
     {
         $this->actingAs($this->admin)
-			->visit('/admin/log-viewer/logs')
-			->see('Logs');
+            ->visit('/admin/log-viewer/logs')
+            ->see('Logs');
     }
 
     public function testLogViewerSingle()
     {
         $this->actingAs($this->admin)
-			->visit('/admin/log-viewer/logs/'.date('Y-m-d'))
-			->see('Log ['.date('Y-m-d').']');
+            ->visit('/admin/log-viewer/logs/'.date('Y-m-d'))
+            ->see('Log ['.date('Y-m-d').']');
     }
 
     public function testLogViewerSingleType()
