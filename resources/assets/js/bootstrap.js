f1e1fa601876cb90db932eb802827e0a5d6103ad
Anthony Rappa <rappa819@gmail.com>
2017-03-15 22:29:17 -0400

    - Fixed route in RedirectIfAuthenticated middleware - Upgrade to laravel commit: fe5146f854a6175233677f83d44519cb32079bd7 - Updated yarn - Reran production files - Update composer

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -38,7 +38,9 @@ window.axios.defaults.headers.common = {
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from "laravel-echo"
+// import Echo from 'laravel-echo'
+
+// window.Pusher = require('pusher-js');
 
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
