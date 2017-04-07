abe75fd1fd29d39bb40d84e3df67af7a9bdfde29
Anthony Rappa <rappa819@gmail.com>
2017-04-06 21:39:38 -0400

    - [ ] Fix RTL View for log-viewer when switching to RTL language - [ ] Update to Laravel commit: f7b9a590f99f7248df49cf35d79b1c1f4cb8ca96

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Laravel 5 Boilerplate',
+    'name' => env('APP_NAME', 'Laravel 5 Boilerplate'),
 
     /*
     |--------------------------------------------------------------------------
