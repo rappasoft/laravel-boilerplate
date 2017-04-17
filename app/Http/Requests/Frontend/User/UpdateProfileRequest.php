01b2b54ef2656e6ee82864b3927b54422fe78717
arrakeen <davz.eu@gmail.com>
2017-04-17 07:42:38 +0200

    Update UpdateProfileRequest.php
    Rules for user's name must be defined as it is in registration's request class

--- app/Http/Requests/Frontend/User/UpdateProfileRequest.php
+++ app/Http/Requests/Frontend/User/UpdateProfileRequest.php
@@ -27,7 +27,7 @@ class UpdateProfileRequest extends Request
     public function rules()
     {
         return [
-            'name'  => 'required',
+            'name'  => 'required|max:255',
             'email' => 'sometimes|required|email',
         ];
     }
