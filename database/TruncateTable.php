f6ebd420bda6924b51262d085dc679623f6ada2f
Anthony Rappa <rappa819@gmail.com>
2017-04-27 20:07:13 -0400

    - Add mssql truncate command.

--- database/TruncateTable.php
+++ database/TruncateTable.php
@@ -23,7 +23,7 @@ trait TruncateTable
             case 'pgsql':
                 return  DB::statement('TRUNCATE TABLE '.$table.' RESTART IDENTITY CASCADE');
 
-            case 'sqlite':
+            case 'sqlite': case 'sqlsrv':
                 return DB::statement('DELETE FROM '.$table);
         }
 
