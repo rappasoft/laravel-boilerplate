3164f69848380e93e3597d9fbaac2fcfe48492b7
Anthony Rappa <rappa819@gmail.com>
2017-05-05 21:13:36 -0400

    no message

--- resources/views/frontend/includes/nav.blade.php
+++ resources/views/frontend/includes/nav.blade.php
@@ -13,7 +13,7 @@
 
         <div class="collapse navbar-collapse" id="frontend-navbar-collapse">
             <ul class="nav navbar-nav">
-                <li>{{ link_to_route('frontend.macros', trans('navs.frontend.macros')) }}</li>
+                <li>{{ link_to_route('frontend.macros', trans('navs.frontend.macros'), [], ['class' => active_class(Active::checkRoute('frontend.macros')) ]) }}</li>
             </ul>
 
             <ul class="nav navbar-nav navbar-right">
