86cd1d71e7ed70cea085fafae25545d3f259e20e
Anthony Rappa <rappa819@gmail.com>
2017-05-07 18:45:04 -0400

    - Update to laravel commit: ba5c29459800e384465c3e5d099df4af3d0598b8

--- config/queue.php
+++ config/queue.php
@@ -2,97 +2,84 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Default Queue Driver
-    |--------------------------------------------------------------------------
-    |
-    | Laravel's queue API supports an assortment of back-ends via a single
-    | API, giving you convenient access to each back-end using the same
-    | syntax for each one. Here you may set the default queue driver.
-    |
-    | Supported: "sync", "database", "beanstalkd", "sqs", "redis", "null"
-    |
-    */
+	/*
+	|--------------------------------------------------------------------------
+	| Default Queue Driver
+	|--------------------------------------------------------------------------
+	|
+	| Laravel's queue API supports an assortment of back-ends via a single
+	| API, giving you convenient access to each back-end using the same
+	| syntax for each one. Here you may set the default queue driver.
+	|
+	| Supported: "sync", "database", "beanstalkd", "sqs", "redis", "null"
+	|
+	*/
 
-    'default' => env('QUEUE_DRIVER', 'sync'),
+	'default' => env('QUEUE_DRIVER', 'sync'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Queue Prefix
-    |--------------------------------------------------------------------------
-    |
-    | If you are running multiple sites on a single server you should consider
-    | specifying a queue prefix. This string will be prepended to the queue
-    | names to prevent cross-talk when using certain local queue drivers.
-    |
-    */
+	/*
+	|--------------------------------------------------------------------------
+	| Queue Connections
+	|--------------------------------------------------------------------------
+	|
+	| Here you may configure the connection information for each server that
+	| is used by your application. A default configuration has been added
+	| for each back-end shipped with Laravel. You are free to add more.
+	|
+	*/
 
-    'prefix' => env('QUEUE_PREFIX', ''),
+	'connections' => [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Queue Connections
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure the connection information for each server that
-    | is used by your application. A default configuration has been added
-    | for each back-end shipped with Laravel. You are free to add more.
-    |
-    */
+		'sync' => [
+			'driver' => 'sync',
+		],
 
-    'connections' => [
+		'database' => [
+			'driver' => 'database',
+			'table' => 'jobs',
+			'queue' => 'default',
+			'retry_after' => 90,
+		],
 
-        'sync' => [
-            'driver' => 'sync',
-        ],
+		'beanstalkd' => [
+			'driver' => 'beanstalkd',
+			'host' => 'localhost',
+			'queue' => 'default',
+			'retry_after' => 90,
+		],
 
-        'database' => [
-            'driver' => 'database',
-            'table' => 'jobs',
-            'queue' => 'default',
-            'retry_after' => 90,
-        ],
+		'sqs' => [
+			'driver' => 'sqs',
+			'key' => 'your-public-key',
+			'secret' => 'your-secret-key',
+			'prefix' => 'https://sqs.us-east-1.amazonaws.com/your-account-id',
+			'queue' => 'your-queue-name',
+			'region' => 'us-east-1',
+		],
 
-        'beanstalkd' => [
-            'driver' => 'beanstalkd',
-            'host' => 'localhost',
-            'queue' => 'default',
-            'retry_after' => 90,
-        ],
+		'redis' => [
+			'driver' => 'redis',
+			'connection' => 'default',
+			'queue' => 'default',
+			'retry_after' => 90,
+		],
 
-        'sqs' => [
-            'driver' => 'sqs',
-            'key' => 'your-public-key',
-            'secret' => 'your-secret-key',
-            'prefix' => 'https://sqs.us-east-1.amazonaws.com/your-account-id',
-            'queue' => 'your-queue-name',
-            'region' => 'us-east-1',
-        ],
+	],
 
-        'redis' => [
-            'driver' => 'redis',
-            'connection' => 'default',
-            'queue' => 'default',
-            'retry_after' => 90,
-        ],
+	/*
+	|--------------------------------------------------------------------------
+	| Failed Queue Jobs
+	|--------------------------------------------------------------------------
+	|
+	| These options configure the behavior of failed queue job logging so you
+	| can control which database and table are used to store the jobs that
+	| have failed. You may change them to any database / table you wish.
+	|
+	*/
 
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Failed Queue Jobs
-    |--------------------------------------------------------------------------
-    |
-    | These options configure the behavior of failed queue job logging so you
-    | can control which database and table are used to store the jobs that
-    | have failed. You may change them to any database / table you wish.
-    |
-    */
-
-    'failed' => [
-        'database' => env('DB_CONNECTION', 'mysql'),
-        'table' => 'failed_jobs',
-    ],
+	'failed' => [
+		'database' => env('DB_CONNECTION', 'mysql'),
+		'table' => 'failed_jobs',
+	],
 
 ];
