1b45a21258fe09a6b9b53257d6d92b5e30e79440
Eric Totten <erictotten@cox.net>
2017-05-12 08:24:33 +0000

    Add contact form

---
+++ app/Http/Requests/Frontend/ContactRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests\Frontend;
+
+use App\Http\Requests\Request;
+
+/**
+ * Class ContactRequest.
+ */
+class ContactRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'                 => 'required|string',
+            'email'                => 'required|string|email',
+            'message'              => 'required|string|min:10',
+            'g-recaptcha-response' => 'required_if:captcha_status,true|captcha',
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'g-recaptcha-response.required_if' => trans('validation.required', ['attribute' => 'captcha']),
+        ];
+    }
+}
---
+++ resources/views/frontend/contact.blade.php
@@ -0,0 +1,85 @@
+@extends('frontend.layouts.app')
+
+@section('content')
+    <div class="row">
+
+        <div class="col-md-8 col-md-offset-2">
+
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
+
+            <div class="panel panel-default">
+                <div class="panel-heading">{{ trans('labels.frontend.contact.contact_box_title') }}</div>
+
+                <div class="panel-body">
+
+                    {{ Form::open(['route' => 'frontend.contact.post', 'class' => 'form-horizontal']) }}
+
+                    <div class="form-group">
+                        {{ Form::label('name', trans('validation.attributes.frontend.your_name'), ['class' => 'col-md-4 control-label']) }}
+                        <div class="col-md-6">
+                            {{ Form::text('name', old('name'),
+                            ['class' => 'form-control', 'required' => 'required', 'autofocus' => 'autofocus', 'placeholder' => trans('validation.attributes.frontend.your_name')]) }}
+                        </div><!--col-md-6-->
+                    </div><!--form-group-->
+
+                    <div class="form-group">
+                        {{ Form::label('email', trans('validation.attributes.frontend.your_email'), ['class' => 'col-md-4 control-label']) }}
+                        <div class="col-md-6">
+                            {{ Form::email('email', old('email'), ['class' => 'form-control', 'required' => 'required', 'placeholder' => trans('validation.attributes.frontend.your_email')]) }}
+                        </div><!--col-md-6-->
+                    </div><!--form-group-->
+
+                    <div class="form-group">
+                        {{ Form::label('message', trans('validation.attributes.frontend.your_message'), ['class' => 'col-md-4 control-label']) }}
+                        <div class="col-md-6">
+                            {{ Form::textarea('message', old('message'), ['class' => 'form-control', 'required' => 'required', 'placeholder' => trans('validation.attributes.frontend.your_message')]) }}
+                        </div><!--col-md-6-->
+                    </div><!--form-group-->
+
+                    @if (config('access.captcha.contact'))
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                {!! Form::captcha() !!}
+                                {{ Form::hidden('captcha_status', 'true') }}
+                            </div><!--col-md-6-->
+                        </div><!--form-group-->
+                    @endif
+
+                    <div class="form-group">
+                        <div class="col-md-6 col-md-offset-4">
+                            <div class="checkbox">
+                                <label>
+                                    {{ Form::checkbox('sendcopy') }} {{ trans('validation.attributes.frontend.sendcopy') }}
+                                </label>
+                            </div>
+                        </div><!--col-md-6-->
+                    </div><!--form-group-->
+
+                    <br />
+
+                    <div class="form-group">
+                        <div class="col-md-6 col-md-offset-4">
+                            {{ Form::submit(trans('labels.frontend.contact.contact_button'), ['class' => 'btn btn-primary']) }}
+                        </div><!--col-md-6-->
+                    </div><!--form-group-->
+
+                    {{ Form::close() }}
+
+                </div><!-- panel body -->
+
+            </div><!-- panel -->
+
+        </div><!-- col-md-8 -->
+
+    </div><!-- row -->
+@endsection
+
+@section('after-scripts')
+    @if (config('access.captcha.contact'))
+        {!! Captcha::script() !!}
+    @endif
+@endsection
