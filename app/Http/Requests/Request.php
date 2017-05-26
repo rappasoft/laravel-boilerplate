601e89feaec781478b0d0c74465382d2b30e195f
Maria Petrescu <mpetrescu@seniorprogramming.ro>
2017-05-26 16:23:42 +0300

    Fix StyleCI analysis

--- app/Http/Requests/Request.php
+++ app/Http/Requests/Request.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
 use App\Exceptions\GeneralException;
+use Illuminate\Foundation\Http\FormRequest;
 
 /**
  * Class Request.
@@ -16,7 +16,6 @@ abstract class Request extends FormRequest
     protected $error = '';
 
     /**
-     *
      * @throws GeneralException
      */
     protected function failedAuthorization()
