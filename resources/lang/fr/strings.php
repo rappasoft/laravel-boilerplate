2ba39f5aab477bc861bae7f71e978280d5bcaf1b
Laurent Napias <tamplan@free.fr>
2017-07-30 22:06:10 +0200

    Apply fixes from StyleCI

--- resources/lang/fr/strings.php
+++ resources/lang/fr/strings.php
@@ -125,7 +125,7 @@ return [
         'general' => [
             'joined'        => 'Membre depuis',
         ],
-          
+
         'user' => [
             'change_email_notice' => "Si vous changez votre adresse email vous serez déconnecté jusqu'à que vous confirmiez votre nouvelle adresse email.",
             'email_changed_notice' => 'Vous devez confirmer votre nouvelle adresse email avant de pouvoir vous connecter de nouveau.',
