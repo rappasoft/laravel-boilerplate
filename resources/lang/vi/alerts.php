532ed3c42c7cb179dd99bf93e617c1f38b865b39
Hân Nguyễn <ngochan.gtf@gmail.com>
2017-08-27 08:30:37 +0700

    Update Vietnamese language to 98%

--- resources/lang/vi/alerts.php
+++ resources/lang/vi/alerts.php
@@ -15,30 +15,30 @@ return [
 
     'backend' => [
         'roles' => [
-            'created' => 'The role was successfully created.',
-            'deleted' => 'The role was successfully deleted.',
-            'updated' => 'The role was successfully updated.',
+            'created' => 'Nhóm quyền đã được thêm mới thành công.',
+            'deleted' => 'Xóa thành công nhóm quyền.',
+            'updated' => 'Nhóm quyền đã được cập nhật.',
         ],
 
         'users' => [
-            'cant_resend_confirmation' => 'The application is currently set to manually approve users.',
-            'confirmation_email'  => 'A new confirmation e-mail has been sent to the address on file.',
-            'confirmed'              => 'The user was successfully confirmed.',
-            'created'             => 'The user was successfully created.',
-            'deleted'             => 'The user was successfully deleted.',
-            'deleted_permanently' => 'The user was deleted permanently.',
-            'restored'            => 'The user was successfully restored.',
-            'session_cleared'      => "The user's session was successfully cleared.",
-            'social_deleted' => 'Social Account Successfully Removed',
-            'unconfirmed' => 'The user was successfully un-confirmed',
-            'updated'             => 'The user was successfully updated.',
-            'updated_password'    => "The user's password was successfully updated.",
+            'cant_resend_confirmation' => 'Hệ thống đã được thiết lập để có thể phê duyệt cách thủ công.',
+            'confirmation_email'  => 'Một e-mail xác thực đã được gửi đến hộp thư mà bạn đăng ký.',
+            'confirmed'              => 'Tài khoản của bạn đã được xác thực.',
+            'created'             => 'Tạo tài khoản thành công.',
+            'deleted'             => 'Tài khoản đã bị xóa.',
+            'deleted_permanently' => 'Tài khoản đã bị xóa vĩnh viễn.',
+            'restored'            => 'Tài khoản đã được phục hồi.',
+            'session_cleared'      => "Đã xóa phiên đăng nhập của thành viên.",
+            'social_deleted' => 'Gỡ bỏ liên kết tài khoản mạng xã hội thành công',
+            'unconfirmed' => 'Hủy kích hoạt tài khoản thành công.',
+            'updated'             => 'Cập nhật tài khoản thành công.',
+            'updated_password'    => "Mật khẩu truy cập đã được thay đổi.",
         ],
     ],
 
     'frontend' => [
         'contact' => [
-            'sent' => 'Your information was successfully sent. We will respond back to the e-mail provided as soon as we can.',
+            'sent' => 'Thông tin liên hệ đã được gửi thành công. Chúng tôi sẽ hồi đáp bạn sớm nhất ngay khi có thể.',
         ],
     ],
 ];
