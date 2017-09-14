35a1ace4888bb07542233daa76866fb88b5cae74
Anthony Rappa <rappa819@gmail.com>
2017-09-13 23:42:13 -0400

    - Change env.

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel 5 Boilerplate'),
+    'name' => env('APP_NAME', 'Laravel ' . app()->version() .' Boilerplate'),
 
     /*
     |--------------------------------------------------------------------------
