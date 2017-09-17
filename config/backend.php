9a05d5104f3f8b15c51b72088f14452e749e0bb6
Anthony Rappa <anthony@rappasoft.com>
2017-09-17 19:35:26 +0000

    Apply fixes from StyleCI

--- config/backend.php
+++ config/backend.php
@@ -1,28 +1,28 @@
 <?php
 
 return [
-	/**
-	 * CoreUI BODY options, add following classes to body to change options
-	 * // Header options
-	 * 1. '.header-fixed'					- Fixed Header
-	 *
-	 * // Sidebar options
-	 * 1. '.sidebar-fixed'					- Fixed Sidebar
-	 * 2. '.sidebar-hidden'				- Hidden Sidebar
-	 * 3. '.sidebar-off-canvas'		    - Off Canvas Sidebar
-	 * 4. '.sidebar-minimized'			    - Minimized Sidebar (Only icons)
-	 * 5. '.sidebar-compact'			    - Compact Sidebar
+    /*
+     * CoreUI BODY options, add following classes to body to change options
+     * // Header options
+     * 1. '.header-fixed'					- Fixed Header
      *
-	 * // Aside options
-	 * 1. '.aside-menu-fixed'			    - Fixed Aside Menu
-	 * 2. '.aside-menu-hidden'			    - Hidden Aside Menu
-	 * 3. '.aside-menu-off-canvas'	        - Off Canvas Aside Menu
+     * // Sidebar options
+     * 1. '.sidebar-fixed'					- Fixed Sidebar
+     * 2. '.sidebar-hidden'				- Hidden Sidebar
+     * 3. '.sidebar-off-canvas'		    - Off Canvas Sidebar
+     * 4. '.sidebar-minimized'			    - Minimized Sidebar (Only icons)
+     * 5. '.sidebar-compact'			    - Compact Sidebar
      *
-	 * // Breadcrumb options
-	 * 1. '.breadcrumb-fixed'			    - Fixed Breadcrumb
+     * // Aside options
+     * 1. '.aside-menu-fixed'			    - Fixed Aside Menu
+     * 2. '.aside-menu-hidden'			    - Hidden Aside Menu
+     * 3. '.aside-menu-off-canvas'	        - Off Canvas Aside Menu
      *
-	 * // Footer options
-	 * 1. '.footer-fixed'					- Fixed footer
-	 */
-	'body_classes' => 'app header-fixed sidebar-fixed aside-menu-fixed aside-menu-hidden',
+     * // Breadcrumb options
+     * 1. '.breadcrumb-fixed'			    - Fixed Breadcrumb
+     *
+     * // Footer options
+     * 1. '.footer-fixed'					- Fixed footer
+     */
+    'body_classes' => 'app header-fixed sidebar-fixed aside-menu-fixed aside-menu-hidden',
 ];
--- routes/breadcrumbs/backend/backend.php
+++ routes/breadcrumbs/backend/backend.php
@@ -1,5 +1,5 @@
 <?php
 
 Breadcrumbs::register('admin.dashboard', function ($breadcrumbs) {
-	$breadcrumbs->push(trans('strings.backend.dashboard.title'), route('admin.dashboard'));
+    $breadcrumbs->push(trans('strings.backend.dashboard.title'), route('admin.dashboard'));
 });
