3ff26e7fd9e286af2cf8817dcba07ca82ed6c221
nasirkhan <nasir8891@gmail.com>
2017-09-19 14:55:28 +0600

    Index View Updated

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,34 +1,46 @@
 @extends('frontend.layouts.app')
 
 @section('content')
-    <div class="row">
-        <div class="col-lg-12 mb-4">
-            <example></example>
-        </div><!-- col-lg-12 -->
 
-        <div class="col-lg-12 mb-4">
-            <div class="card">
-                <div class="card-header">
-                    <i class="fa fa-home"></i> {{ __('navs.general.home') }}
-                </div>
-                <div class="card-body">
-                    {{ __('strings.frontend.welcome_to', ['place' => app_name()]) }}
-                </div>
-            </div><!--card-->
-        </div><!-- col-lg-12 -->
 
-        <div class="col-lg-12 mb-4">
-            <div class="card">
-                <div class="card-header">
-                    <i class="fa fa-home"></i> Font Awesome {{ __('strings.frontend.test') }}
-                </div>
-                <div class="card-body">
-                    <i class="fa fa-home"></i>
-                    <i class="fa fa-facebook"></i>
-                    <i class="fa fa-twitter"></i>
-                    <i class="fa fa-pinterest"></i>
-                </div>
-            </div><!--card-->
-        </div><!-- col-lg-12 -->
-    </div><!--row-->
+<!-- Homepage Welcome Block -->
+<div class="row mb-4">
+    <div class="col">
+        <div class="card">
+            <div class="card-header">
+                <i class="fa fa-home"></i> {{ __('navs.general.home') }}
+            </div>
+            <div class="card-body">
+                {{ __('strings.frontend.welcome_to', ['place' => app_name()]) }}
+            </div>
+        </div>
+    </div>
+</div>
+<!-- / Homepage Welcome Block -->
+
+<!-- Vue Component Example -->
+<div class="row mb-4">
+    <div class="col">
+        <example></example>
+    </div>
+</div>
+<!-- / Vue Component Example -->
+
+<!-- Font Awesome Example -->
+<div class="row">
+    <div class="col">
+        <div class="card">
+            <div class="card-header">
+                <i class="fa fa-fort-awesome"></i> Font Awesome {{ __('strings.frontend.test') }}
+            </div>
+            <div class="card-body">
+                <i class="fa fa-home"></i>
+                <i class="fa fa-facebook"></i>
+                <i class="fa fa-twitter"></i>
+                <i class="fa fa-pinterest"></i>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- / Font Awesome Example -->
 @endsection
