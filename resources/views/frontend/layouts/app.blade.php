61e98f884640d85de2f9ffeab280af03e10357f0
nasirkhan <nasir8891@gmail.com>
2017-09-19 16:29:10 +0600

    Account page restructure

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -1,43 +1,43 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title', app_name())</title>
-    <meta name="description" content="@yield('meta_description', 'Laravel 5 Boilerplate')">
-    <meta name="author" content="@yield('meta_author', 'Anthony Rappa')">
-    @yield('meta')
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>@yield('title', app_name())</title>
+        <meta name="description" content="@yield('meta_description', 'Laravel 5 Boilerplate')">
+        <meta name="author" content="@yield('meta_author', 'Anthony Rappa')">
+        @yield('meta')
 
-    {{-- See https://laravel.com/docs/5.5/blade#stacks for usage --}}
-    @stack('before-styles')
+        {{-- See https://laravel.com/docs/5.5/blade#stacks for usage --}}
+        @stack('before-styles')
 
-    <!-- Check if the language is set to RTL, so apply the RTL layouts -->
-    <!-- Otherwise apply the normal LTR layouts -->
-    @langrtl
-        {{ Html::style(getRtlCss(mix('css/frontend.css'))) }}
-    @else
-        {{ Html::style(mix('css/frontend.css')) }}
-    @endlangrtl
+        <!-- Check if the language is set to RTL, so apply the RTL layouts -->
+        <!-- Otherwise apply the normal LTR layouts -->
+        @langrtl
+            {{ Html::style(getRtlCss(mix('css/frontend.css'))) }}
+        @else
+            {{ Html::style(mix('css/frontend.css')) }}
+        @endlangrtl
 
-    @stack('after-styles')
-</head>
-<body>
-    <div id="app">
-        @include('includes.partials.logged-in-as')
-        @include('frontend.includes.nav')
+        @stack('after-styles')
+    </head>
+    <body>
+        <div id="app">
+            @include('includes.partials.logged-in-as')
+            @include('frontend.includes.nav')
 
-        <div class="container">
-            @include('includes.partials.messages')
-            @yield('content')
-        </div><!-- container -->
-    </div><!-- #app -->
+            <div class="container">
+                @include('includes.partials.messages')
+                @yield('content')
+            </div><!-- container -->
+        </div><!-- #app -->
 
-    <!-- Scripts -->
-    @stack('before-scripts')
-    {!! Html::script(mix('js/frontend.js')) !!}
-    @stack('after-scripts')
+        <!-- Scripts -->
+        @stack('before-scripts')
+        {!! Html::script(mix('js/frontend.js')) !!}
+        @stack('after-scripts')
 
-    @include('includes.partials.ga')
-</body>
+        @include('includes.partials.ga')
+    </body>
 </html>
--- resources/views/frontend/user/dashboard.blade.php
+++ resources/views/frontend/user/dashboard.blade.php
@@ -1,153 +1,147 @@
 @extends('frontend.layouts.app')
 
 @section('content')
-    <div class="row mb-5">
+<div class="row mb-4">
+    <div class="col">
+        <div class="card">
 
-        <div class="col-xs-12">
+            <div class="card-header">
+                <strong>
+                    <i class="fa fa-dashboard"></i> {{ __('navs.frontend.dashboard') }}
+                </strong>
+            </div><!--card-header-->
 
-            <div class="card">
+            <div class="card-body">
+                <div class="row">
 
-                <div class="card-header">
-                    {{ __('navs.frontend.dashboard') }}
-                </div><!--card-header-->
+                    <div class="col col-sm-4 order-1 order-sm-2  mb-4">
 
-                <div class="card-body">
+                        <div class="card mb-4 bg-light">
+                            <img class="card-img-top" src="{{ $logged_in_user->picture }}" alt="Profile Picture">
 
-                    <div class="row">
+                            <div class="card-body">
+                                <h4 class="card-title">
+                                    {{ $logged_in_user->name }}<br/>
+                                </h4>
 
-                        <div class="col-md-4 order-1 order-sm-2">
+                                <p class="card-text">
+                                    <small>
+                                        <i class="fa fa-envelope-o"></i> {{ $logged_in_user->email }}<br/>
+                                        <i class="fa fa-calendar-check-o"></i> {{ __('strings.frontend.general.joined') }} {{ $logged_in_user->created_at->format('F jS, Y') }}
+                                    </small>
+                                </p>
 
-                            <div class="card mb-4">
-                                <img class="card-img-top" src="{{ $logged_in_user->picture }}" alt="Profile Picture">
+                                <p class="card-text">
 
-                                <div class="card-body">
-                                    <h4 class="card-title">
-                                        {{ $logged_in_user->name }}<br/>
-                                    </h4>
+                                    <a href="{{ route ('frontend.user.account')}}" class="btn btn-info btn-sm mb-1">
+                                        <i class="fa fa-user-circle-o"></i> {{ __('navs.frontend.user.account') }}
+                                    </a>
 
-                                    <p class="card-text">
-                                        <small>
-                                            {{ $logged_in_user->email }}<br/>
-                                            {{ __('strings.frontend.general.joined') }} {{ $logged_in_user->created_at->format('F jS, Y') }}
-                                        </small>
-                                    </p>
-
-                                    <p class="card-text">
-                                        {{ link_to_route('frontend.user.account', __('navs.frontend.user.account'), [], ['class' => 'btn btn-info btn-sm']) }}
-
-                                        @can('view backend')
-                                            {{ link_to_route('admin.dashboard', __('navs.frontend.user.administration'), [], ['class' => 'btn btn-danger btn-sm']) }}
-                                        @endcan
-                                    </p>
-                                </div>
+                                    @can('view backend')
+                                    <a href="{{ route ('admin.dashboard')}}" class="btn btn-danger btn-sm mb-1">
+                                        <i class="fa fa-user-secret"></i> {{ __('navs.frontend.user.administration') }}
+                                    </a>
+                                    @endcan
+                                </p>
                             </div>
+                        </div>
 
-                            <div class="card">
-                                <div class="card-header">
-                                    Sidebar Item
-                                </div><!--card-header-->
+                        <div class="card border-info mb-4">
+                            <div class="card-header">Header</div>
+                            <div class="card-body text-info">
+                                <h4 class="card-title">Info card title</h4>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
 
-                                <div class="card-body">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non qui facilis deleniti expedita fuga ipsum numquam aperiam itaque cum maxime.
-                                </div><!--card-body-->
-                            </div><!--card-->
-                        </div><!--col-md-4-->
+                        <div class="card bg-primary">
+                            <div class="card-header">
+                                Sidebar Item
+                            </div><!--card-header-->
 
-                        <div class="col-md-8 order-2 order-sm-1">
-                            <div class="card mb-4">
-                                <div class="card-header">
-                                    Item
-                                </div><!--card-header-->
+                            <div class="card-body">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non qui facilis deleniti expedita fuga ipsum numquam aperiam itaque cum maxime.
+                            </div><!--card-body-->
+                        </div><!--card-->
+                    </div><!--col-md-4-->
 
-                                <div class="card-body">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non qui facilis deleniti expedita fuga ipsum numquam aperiam itaque cum maxime.
-                                </div><!--card-body-->
-                            </div><!--card-->
+                    <div class="col-md-8 order-2 order-sm-1">
+                        <div class="card mb-4">
+                            <div class="card-header">
+                                Custom Background
+                            </div><!--card-header-->
 
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="card mb-4">
-                                        <div class="card-header">
-                                            Item
-                                        </div><!--card-header-->
+                            <div class="card-body">
+                                <div class="p-3 mb-2 bg-primary text-white">.bg-primary</div>
+                                <div class="p-3 mb-2 bg-secondary text-white">.bg-secondary</div>
+                                <div class="p-3 mb-2 bg-success text-white">.bg-success</div>
+                                <div class="p-3 mb-2 bg-danger text-white">.bg-danger</div>
+                                <div class="p-3 mb-2 bg-warning text-white">.bg-warning</div>
+                                <div class="p-3 mb-2 bg-info text-white">.bg-info</div>
+                                <div class="p-3 mb-2 bg-light text-gray-dark">.bg-light</div>
+                                <div class="p-3 mb-2 bg-dark text-white">.bg-dark</div>
+                                <div class="p-3 mb-2 bg-white text-gray-dark">.bg-white</div>
+                            </div><!--card-body-->
+                        </div><!--card-->
 
-                                        <div class="card-body">
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non qui facilis deleniti expedita fuga ipsum numquam aperiam itaque cum maxime.
-                                        </div><!--card-body-->
-                                    </div><!--card-->
-                                </div><!--col-md-6-->
+                        <div class="row">
+                            <div class="col">
+                                <div class="card mb-4">
+                                    <div class="card-header">
+                                        Item
+                                    </div><!--card-header-->
 
-                                <div class="col-md-6">
-                                    <div class="card mb-4">
-                                        <div class="card-header">
-                                            Item
-                                        </div><!--card-header-->
+                                    <div class="card-body">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non qui facilis deleniti expedita fuga ipsum numquam aperiam itaque cum maxime.
+                                    </div><!--card-body-->
+                                </div><!--card-->
+                            </div><!--col-md-6-->
 
-                                        <div class="card-body">
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non qui facilis deleniti expedita fuga ipsum numquam aperiam itaque cum maxime.
-                                        </div><!--card-body-->
-                                    </div><!--card-->
-                                </div><!--col-md-6-->
+                            <div class="col">
+                                <div class="card mb-4">
+                                    <div class="card-header">
+                                        Item
+                                    </div><!--card-header-->
 
-                                <div class="col-md-6">
-                                    <div class="card mb-4">
-                                        <div class="card-header">
-                                            Item
-                                        </div><!--card-header-->
+                                    <div class="card-body">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non qui facilis deleniti expedita fuga ipsum numquam aperiam itaque cum maxime.
+                                    </div><!--card-body-->
+                                </div><!--card-->
+                            </div><!--col-md-6-->
 
-                                        <div class="card-body">
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non qui facilis deleniti expedita fuga ipsum numquam aperiam itaque cum maxime.
-                                        </div><!--card-body-->
-                                    </div><!--card-->
-                                </div><!--col-md-6-->
+                            <div class="w-100"></div>
 
-                                <div class="col-md-6">
-                                    <div class="card mb-4">
-                                        <div class="card-header">
-                                            Item
-                                        </div><!--card-header-->
+                            <div class="col">
+                                <div class="card mb-4">
+                                    <div class="card-header">
+                                        Item
+                                    </div><!--card-header-->
 
-                                        <div class="card-body">
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non qui facilis deleniti expedita fuga ipsum numquam aperiam itaque cum maxime.
-                                        </div><!--card-body-->
-                                    </div><!--card-->
-                                </div><!--col-md-6-->
+                                    <div class="card-body">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non qui facilis deleniti expedita fuga ipsum numquam aperiam itaque cum maxime.
+                                    </div><!--card-body-->
+                                </div><!--card-->
+                            </div><!--col-md-6-->
 
-                                <div class="col-md-6">
-                                    <div class="card">
-                                        <div class="card-header">
-                                            Item
-                                        </div><!--card-header-->
+                            <div class="col">
+                                <div class="card mb-4">
+                                    <div class="card-header">
+                                        Item
+                                    </div><!--card-header-->
 
-                                        <div class="card-body">
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non qui facilis deleniti expedita fuga ipsum numquam aperiam itaque cum maxime.
-                                        </div><!--card-body-->
-                                    </div><!--card-->
-                                </div><!--col-md-6-->
+                                    <div class="card-body">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non qui facilis deleniti expedita fuga ipsum numquam aperiam itaque cum maxime.
+                                    </div><!--card-body-->
+                                </div><!--card-->
+                            </div><!--col-md-6-->
 
-                                <div class="col-md-6">
-                                    <div class="card">
-                                        <div class="card-header">
-                                            Item
-                                        </div><!--card-header-->
+                        </div><!--row-->
 
-                                        <div class="card-body">
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non qui facilis deleniti expedita fuga ipsum numquam aperiam itaque cum maxime.
-                                        </div><!--card-body-->
-                                    </div><!--card-->
-                                </div><!--col-md-6-->
+                    </div><!--col-md-8-->
+                </div>
+            </div> <!-- / card-body -->
+        </div><!-- / card -->
+    </div><!-- / row -->
+</div>
 
-                            </div><!--row-->
-
-                        </div><!--col-md-8-->
-
-                    </div><!--row-->
-
-                </div><!--card-body-->
-
-            </div><!--card-->
-
-        </div><!-- col-md-10 -->
-
-    </div><!-- row -->
 @endsection
