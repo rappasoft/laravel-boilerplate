7c86ca670425c78e31ae33d33ffab1375f441baa
nasirkhan <nasir8891@gmail.com>
2017-10-08 09:47:39 +0600

    ^ role edit

--- resources/views/backend/auth/role/edit.blade.php
+++ resources/views/backend/auth/role/edit.blade.php
@@ -3,56 +3,96 @@
 @section ('title', __('labels.backend.access.roles.management') . ' | ' . __('labels.backend.access.roles.edit'))
 
 @section('page-header')
-    <h5 class="mb-4">{{ __('labels.backend.access.roles.management') }}
+    <!-- <h5 class="mb-4">{{ __('labels.backend.access.roles.management') }}
         <small class="text-muted">{{ __('labels.backend.access.roles.edit') }}</small>
-    </h5>
+    </h5> -->
 @endsection
 
 @section('content')
-    <form action="{{ route('admin.auth.role.update', $role) }}" method="post" class="form-horizontal">
-        {{ csrf_field() }}
-        {{ method_field('PATCH') }}
+<div class="card">
+    <div class="card-body">
+        <div class="row">
+            <div class="col-sm-5">
+                <h4 class="card-title mb-0">
+                    {{ __('labels.backend.access.roles.management') }}
+                    <small class="text-muted">{{ __('labels.backend.access.roles.edit') }}</small>
+                </h4>
 
-        <div class="card">
-            <div class="card-header">
-                {{ __('labels.backend.access.roles.edit') }}
-            </div><!-- card-header -->
+                <div class="small text-muted">
+                    Roles Management Dashboard
+                </div>
+            </div>
+            <!--/.col-->
+            <div class="col-sm-7">
+                <div class="btn-toolbar float-right" role="toolbar" aria-label="Toolbar with button groups">
+                    <button onclick="window.history.back();"class="btn btn-warning ml-1"><i class="fa fa-reply"></i></button>
+                </div>
+            </div>
+            <!--/.col-->
+        </div>
+        <!--/.row-->
 
-            <div class="card-body">
-                <div class="form-group row">
-                    <label class="col-md-2 form-control-label" for="name">
-                        {{ __('validation.attributes.backend.access.roles.name') }}
-                    </label>
+        <hr>
 
-                    <div class="col-md-10">
-                        <input type="text" id="name" name="name" class="form-control" placeholder="{{ __('validation.attributes.backend.access.roles.name') }}" value="{{ $role->name }}" maxlength="191" required="required" autofocus="autofocus">
+        <div class="row mt-4">
+            <div class="col">
+                <form action="{{ route('admin.auth.role.update', $role) }}" method="post" class="form-horizontal">
+                    {{ csrf_field() }}
+                    {{ method_field('PATCH') }}
+
+
+                    <div class="card-body">
+                        <div class="form-group row">
+                            <label class="col-md-2 form-control-label" for="name">
+                                {{ __('validation.attributes.backend.access.roles.name') }}
+                            </label>
+
+                            <div class="col-md-10">
+                                <input type="text" id="name" name="name" class="form-control" placeholder="{{ __('validation.attributes.backend.access.roles.name') }}" value="{{ $role->name }}" maxlength="191" required="required" autofocus="autofocus">
+                            </div>
+                        </div><!--form-group-->
+
+                        <div class="form-group row">
+                            <label class="col-md-2 form-control-label" for="name">
+                                {{ __('validation.attributes.backend.access.roles.associated_permissions') }}
+                            </label>
+
+                            <div class="col-md-10">
+                                @if ($permissions->count())
+                                    @foreach($permissions as $permission)
+                                        <div class="checkbox">
+                                            <label for="permission-{{ $permission->id }}">
+                                                <input type="checkbox" name="permissions[]" value="{{ $permission->name }}" id="permission-{{ $permission->id }}" {{ in_array($permission->name, $rolePermissions) ? 'checked="checked"' : '' }} />
+                                                {{ ucwords($permission->name) }}
+                                            </label>
+                                        </div>
+                                    @endforeach
+                                @endif
+                            </div>
+                        </div><!--form-group-->
+                    </div><!-- card-body -->
+
+                    <div class="row">
+                        <div class="col">
+                            {{ form_cancel(route('admin.auth.role.index'), __('buttons.general.cancel')) }}
+                            {{ form_submit(__('buttons.general.crud.update')) }}
+                        </div>
                     </div>
-                </div><!--form-group-->
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- /.card-body -->
+    <div class="card-footer">
+        <div class="row">
+            <div class="col">
+                <small class="float-right text-muted">
+                    Updated: {{$role->updated_at->diffForHumans()}},
+                    Created at: {{$role->created_at->toCookieString()}}
+                </small>
+            </div>
+        </div>
+    </div>
+</div>
 
-                <div class="form-group row">
-                    <label class="col-md-2 form-control-label" for="name">
-                        {{ __('validation.attributes.backend.access.roles.associated_permissions') }}
-                    </label>
-
-                    <div class="col-md-10">
-                        @if ($permissions->count())
-                            @foreach($permissions as $permission)
-                                <div class="checkbox">
-                                    <label for="permission-{{ $permission->id }}">
-                                        <input type="checkbox" name="permissions[]" value="{{ $permission->name }}" id="permission-{{ $permission->id }}" {{ in_array($permission->name, $rolePermissions) ? 'checked="checked"' : '' }} />
-                                        {{ ucwords($permission->name) }}
-                                    </label>
-                                </div>
-                            @endforeach
-                        @endif
-                    </div>
-                </div><!--form-group-->
-            </div><!-- card-body -->
-
-            <div class="card-footer">
-                {{ form_cancel(route('admin.auth.role.index'), __('buttons.general.cancel')) }}
-                {{ form_submit(__('buttons.general.crud.update')) }}
-            </div><!--card-footer-->
-        </div><!--card-->
-    </form>
 @endsection
