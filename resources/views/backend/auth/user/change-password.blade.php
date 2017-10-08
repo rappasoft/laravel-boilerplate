23d1eb94ba3177f3e4d8536a96c6d95b1b2c9040
nasirkhan <nasir8891@gmail.com>
2017-10-08 10:09:08 +0600

    ^ change password

--- resources/views/backend/auth/user/change-password.blade.php
+++ resources/views/backend/auth/user/change-password.blade.php
@@ -7,47 +7,83 @@
 @endsection
 
 @section('page-header')
-    <h5 class="mb-4">{{ __('labels.backend.access.users.management') }}
+    <!-- <h5 class="mb-4">{{ __('labels.backend.access.users.management') }}
         <small class="text-muted">{{ __('labels.backend.access.users.change_password') }}</small>
-    </h5>
+    </h5> -->
 @endsection
 
 @section('content')
-    <form action="{{ route('admin.auth.user.change-password.post', $user) }}" method="post" class="form-horizontal">
-        {{ csrf_field() }}
-        {{ method_field('PATCH') }}
+<div class="card">
+    <div class="card-body">
+        <div class="row">
+            <div class="col-sm-5">
+                <h4 class="card-title mb-0">
+                    {{ __('labels.backend.access.users.management') }}
+                    <small class="text-muted">{{ __('labels.backend.access.users.change_password') }}</small>
+                </h4>
+                <div class="small text-muted">
+                    {{ __('labels.backend.access.users.change_password_for', ['user' => $user->name]) }}
+                </div>
+            </div>
+            <!--/.col-->
+            <div class="col-sm-7">
+                <div class="btn-toolbar float-right" role="toolbar" aria-label="Toolbar with button groups">
+                    <button onclick="window.history.back();"class="btn btn-warning ml-1" data-toggle="tooltip" title="Return Back"><i class="fa fa-reply"></i></button>
+                </div>
+            </div>
+            <!--/.col-->
+        </div>
+        <!--/.row-->
+        <hr>
+        <div class="row mt-4 mb-4">
+            <div class="col">
+                <form action="{{ route('admin.auth.user.change-password.post', $user) }}" method="post" class="form-horizontal">
+                    {{ csrf_field() }}
+                    {{ method_field('PATCH') }}
 
-        <div class="card">
-            <div class="card-header">
-                {{ __('labels.backend.access.users.change_password_for', ['user' => $user->name]) }}
-            </div><!-- card-header -->
+                    <div class="form-group row">
+                        <label class="col-md-2 form-control-label" for="password">
+                            {{ __('validation.attributes.backend.access.users.password') }}
+                        </label>
 
-            <div class="card-body">
-                <div class="form-group row">
-                    <label class="col-md-2 form-control-label" for="password">
-                        {{ __('validation.attributes.backend.access.users.password') }}
-                    </label>
+                        <div class="col-md-10">
+                            <input type="password" id="password" name="password" class="form-control" placeholder="{{ __('validation.attributes.backend.access.users.password') }}" required="required">
+                        </div>
+                    </div><!--form-group-->
 
-                    <div class="col-md-10">
-                        <input type="password" id="password" name="password" class="form-control" placeholder="{{ __('validation.attributes.backend.access.users.password') }}" required="required" autofocus="autofocus">
+                    <div class="form-group row">
+                        <label class="col-md-2 form-control-label" for="password_confirmation">
+                            {{ __('validation.attributes.backend.access.users.password_confirmation') }}
+                        </label>
+
+                        <div class="col-md-10">
+                            <input type="password" id="password_confirmation" name="password_confirmation" class="form-control" placeholder="{{ __('validation.attributes.backend.access.users.password_confirmation') }}" required="required">
+                        </div>
+                    </div><!--form-group-->
+
+                    <div class="row">
+                        <div class="col">
+                            {{ form_cancel(route('admin.auth.user.index'), __('buttons.general.cancel')) }}
+                            {{ form_submit(__('buttons.general.crud.update')) }}
+                        </div>
                     </div>
-                </div><!--form-group-->
+                    <!-- /.row -->
+                </form>
+            </div>
+            <!--/.col-->
+        </div>
+        <!--/.row-->
+    </div>
+    <div class="card-footer">
+        <div class="row">
+            <div class="col">
+                <small class="float-right text-muted">
+                    Updated: {{$user->updated_at->diffForHumans()}},
+                    Created at: {{$user->created_at->toCookieString()}}
+                </small>
+            </div>
+        </div>
+    </div>
+</div>
 
-                <div class="form-group row">
-                    <label class="col-md-2 form-control-label" for="password_confirmation">
-                        {{ __('validation.attributes.backend.access.users.password_confirmation') }}
-                    </label>
-
-                    <div class="col-md-10">
-                        <input type="password" id="password_confirmation" name="password_confirmation" class="form-control" placeholder="{{ __('validation.attributes.backend.access.users.password_confirmation') }}" required="required">
-                    </div>
-                </div><!--form-group-->
-            </div><!-- card-body -->
-
-            <div class="card-footer">
-                {{ form_cancel(route('admin.auth.user.index'), __('buttons.general.cancel')) }}
-                {{ form_submit(__('buttons.general.crud.update')) }}
-            </div><!--card-footer-->
-        </div><!--card-->
-    </form>
 @endsection
