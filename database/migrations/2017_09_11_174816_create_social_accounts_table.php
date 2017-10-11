85302d003140dcc0dd399995eca435f9f4ba501b
Anthony Rappa <rappa819@gmail.com>
2017-10-11 10:56:27 -0400

    - Remove unneeded

--- database/migrations/2017_09_11_174816_create_social_accounts_table.php
+++ database/migrations/2017_09_11_174816_create_social_accounts_table.php
@@ -17,7 +17,7 @@ class CreateSocialAccountsTable extends Migration
     public function up()
     {
         Schema::create('social_accounts', function (Blueprint $table) {
-            $table->increments('id')->unsigned();
+            $table->increments('id');
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
             $table->string('provider', 32);
