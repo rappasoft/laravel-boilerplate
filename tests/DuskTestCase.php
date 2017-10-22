ac99a933234441f4fedf3ff1ed28a3c7039b8f6c
Anthony Rappa <anthony@rappasoft.com>
2017-10-22 03:28:47 +0000

    Apply fixes from StyleCI

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -31,7 +31,7 @@ abstract class DuskTestCase extends BaseTestCase
     {
         $options = (new ChromeOptions)->addArguments([
             '--disable-gpu',
-            '--headless'
+            '--headless',
         ]);
 
         return RemoteWebDriver::create(
