4700acfb60be015362ad9535aebe6b2255ff249e
Anthony Rappa <rappa819@gmail.com>
2017-10-23 16:20:50 -0400

    - Updated zh translations. - Removed unused history translations

--- resources/lang/zh/navs.php
+++ resources/lang/zh/navs.php
@@ -2,35 +2,35 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Navs Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used in menu items throughout the system.
-    | Regardless where it is placed, a menu item can be listed here so it is easily
-    | found in a intuitive way.
-    |
-    */
+	/*
+	|--------------------------------------------------------------------------
+	| Navs Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used in menu items throughout the system.
+	| Regardless where it is placed, a menu item can be listed here so it is easily
+	| found in a intuitive way.
+	|
+	*/
 
-    'general' => [
-        'home'   => '首页',
-        'logout' => '退出',
-    ],
+	'general' => [
+		'home'   => '首页',
+		'logout' => '退出',
+	],
 
-    'frontend' => [
-        'contact' => 'Contact',
-        'dashboard' => '指示板',
-        'login'     => '登录',
-        'macros'    => '宏',
-        'register'  => '注册',
+	'frontend' => [
+		'contact' => '联系',
+		'dashboard' => '指示板',
+		'login'     => '登录',
+		'macros'    => '宏',
+		'register'  => '注册',
 
-        'user' => [
-            'account'         => '我的账户',
-            'administration'  => '管理',
-            'change_password' => '更改密码',
-            'my_information'  => '我的信息',
-            'profile'         => '个人资料',
-        ],
-    ],
+		'user' => [
+			'account'         => '我的账户',
+			'administration'  => '管理',
+			'change_password' => '更改密码',
+			'my_information'  => '我的信息',
+			'profile'         => '个人资料',
+		],
+	],
 ];
