95cf4f37402c6cad4e10d78e8fd70c7c0732d909
Anthony Rappa <anthony@rappasoft.com>
2017-10-27 01:57:14 +0000

    Apply fixes from StyleCI

--- tests/Browser/Frontend/Routes/LoggedInRouteTest.php
+++ tests/Browser/Frontend/Routes/LoggedInRouteTest.php
@@ -2,78 +2,75 @@
 
 namespace Tests\Browser\Frontend\Routes;
 
-use App\Events\Frontend\Auth\UserLoggedOut;
-use Illuminate\Support\Facades\Event;
-use Tests\Browser\Pages\Frontend\HomePage;
-use Tests\Browser\Pages\Frontend\User\Account;
-use Tests\Browser\Pages\Frontend\User\Dashboard;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
+use Illuminate\Support\Facades\Event;
+use Tests\Browser\Pages\Frontend\HomePage;
+use App\Events\Frontend\Auth\UserLoggedOut;
+use Tests\Browser\Pages\Frontend\User\Account;
+use Tests\Browser\Pages\Frontend\User\Dashboard;
 
 /**
- * Class DashboardTest
- *
- * @package Tests\Browser\Frontend\Routes
+ * Class DashboardTest.
  */
-class DashboardTest extends DuskTestCase
+class LoggedInRouteTest extends DuskTestCase
 {
+    public function testHomePageLoggedIn()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs($this->user)
+                ->visit(new HomePage)
+                ->assertSee('Dashboard')
+                ->assertSee($this->user->name)
+                ->assertDontSee('Administration');
+        });
+    }
 
-	public function testHomePageLoggedIn()
-	{
-		$this->browse(function (Browser $browser) {
-			$browser->loginAs($this->user)
-				->visit(new HomePage)
-				->assertSee('Dashboard')
-				->assertSee($this->user->name)
-				->assertDontSee('Administration');
-		});
-	}
+    public function testDashboardPage()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs($this->user)
+                ->visit(new Dashboard)
+                ->assertSee('Joined')
+                ->assertSee($this->user->name)
+                ->assertDontSee('Administration');
+        });
+    }
 
-	public function testDashboardPage()
-	{
-		$this->browse(function (Browser $browser) {
-			$browser->loginAs($this->user)
-				->visit(new Dashboard)
-				->assertSee('Joined')
-				->assertSee($this->user->name)
-				->assertDontSee('Administration');
-		});
-	}
+    public function testAccountPage()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs($this->user)
+                ->visit(new Account)
+                ->assertSee('My Account')
+                ->assertSee('Profile')
+                ->assertSee('Update Information')
+                ->assertSee('Change Password')
+                ->assertDontSee('Administration');
+        });
+    }
 
-	public function testAccountPage()
-	{
-		$this->browse(function (Browser $browser) {
-			$browser->loginAs($this->user)
-				->visit(new Account)
-				->assertSee('My Account')
-				->assertSee('Profile')
-				->assertSee('Update Information')
-				->assertSee('Change Password')
-				->assertDontSee('Administration');
-		});
-	}
+    public function testLoggedInAdmin()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs($this->admin)
+                ->visit(new HomePage)
+                ->assertSee('Administration')
+                ->assertSee($this->admin->name);
+        });
+    }
 
-	public function testLoggedInAdmin()
-	{
-		$this->browse(function (Browser $browser) {
-			$browser->loginAs($this->admin)
-				->visit(new HomePage)
-				->assertSee('Administration')
-				->assertSee($this->admin->name);
-		});
-	}
+    public function testLogoutRoute()
+    {
+        Event::fake();
 
-	public function testLogoutRoute()
-	{
-		Event::fake();
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs($this->user)
+                ->visit('/logout')
+                ->assertSee('Login')
+                ->assertSee('Register');
+        });
 
-		$this->browse(function (Browser $browser) {
-			$browser->loginAs($this->user)
-				->visit('/logout')
-				->assertSee('Login')
-				->assertSee('Register');
-		});
-
-		Event::assertDispatched(UserLoggedOut::class);
-	}
+        Event::assertDispatched(UserLoggedOut::class);
+    }
 }
