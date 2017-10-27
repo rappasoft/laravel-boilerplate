ed32b65d032fdfa325f60ff837f27741886c2a2d
Anthony Rappa <anthony@rappasoft.com>
2017-10-27 01:47:01 +0000

    Apply fixes from StyleCI

--- tests/Browser/Frontend/Routes/LoggedInRouteTest.php
+++ tests/Browser/Frontend/Routes/LoggedInRouteTest.php
@@ -2,38 +2,35 @@
 
 namespace Tests\Browser\Frontend\Routes;
 
-use Tests\Browser\Pages\Frontend\HomePage;
-use Tests\Browser\Pages\Frontend\User\Dashboard;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
+use Tests\Browser\Pages\Frontend\HomePage;
+use Tests\Browser\Pages\Frontend\User\Dashboard;
 
 /**
- * Class DashboardTest
- *
- * @package Tests\Browser\Frontend\Routes
+ * Class DashboardTest.
  */
-class DashboardTest extends DuskTestCase
+class LoggedInRouteTest extends DuskTestCase
 {
+    public function testHomePageLoggedIn()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs($this->user)
+                ->visit(new HomePage)
+                ->assertSee('Dashboard')
+                ->assertSee($this->user->name)
+                ->assertDontSee('Administration');
+        });
+    }
 
-	public function testHomePageLoggedIn()
-	{
-		$this->browse(function (Browser $browser) {
-			$browser->loginAs($this->user)
-				->visit(new HomePage)
-				->assertSee('Dashboard')
-				->assertSee($this->user->name)
-				->assertDontSee('Administration');
-		});
-	}
-
-	public function testDashboardPage()
-	{
-		$this->browse(function (Browser $browser) {
-			$browser->loginAs($this->user)
-				->visit(new Dashboard)
-				->assertSee('Joined')
-				->assertSee($this->user->name)
-				->assertDontSee('Administration');
-		});
-	}
+    public function testDashboardPage()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs($this->user)
+                ->visit(new Dashboard)
+                ->assertSee('Joined')
+                ->assertSee($this->user->name)
+                ->assertDontSee('Administration');
+        });
+    }
 }
