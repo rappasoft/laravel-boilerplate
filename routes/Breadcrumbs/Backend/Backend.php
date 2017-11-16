565a42342ddcf9831174a633ad018c0a2e6091e8
vagrant <vagrant@homestead>
2017-11-16 19:50:10 +0000

    git cache cleared

--- routes/Breadcrumbs/Backend/Backend.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-Breadcrumbs::register('admin.dashboard', function ($breadcrumbs) {
-    $breadcrumbs->push(__('strings.backend.dashboard.title'), route('admin.dashboard'));
-});
-
-require __DIR__.'/auth.php';
-require __DIR__.'/log-viewer.php';
