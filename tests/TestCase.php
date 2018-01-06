3a7ce91b8fc3265ac64d5a388cc68e00dfd7780d
Manuel Christlieb <manuel@christlieb.eu>
2018-01-06 01:04:08 +0100

    set multiple testsuites in phpunit and add first feature test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,102 +1,14 @@
 <?php
 
-use App\Models\Auth\Role;
-use App\Models\Auth\User;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+namespace Tests;
+
+
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 /**
  * Class TestCase.
  */
-abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
+abstract class TestCase extends BaseTestCase
 {
-    use DatabaseTransactions,
-        DatabaseMigrations;
-
-    /**
-     * The base URL to use while testing the application.
-     *
-     * @var string
-     */
-    protected $baseUrl = 'http://l5boilerplate.dev';
-
-    /**
-     * @var
-     */
-    protected $admin;
-
-    /**
-     * @var
-     */
-    protected $executive;
-
-    /**
-     * @var
-     */
-    protected $user;
-
-    /**
-     * @var
-     */
-    protected $adminRole;
-
-    /**
-     * @var
-     */
-    protected $executiveRole;
-
-    /**
-     * @var
-     */
-    protected $userRole;
-
-    /**
-     * Creates the application.
-     *
-     * @return \Illuminate\Foundation\Application
-     */
-    public function createApplication()
-    {
-        $app = require __DIR__.'/../bootstrap/app.php';
-
-        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-        return $app;
-    }
-
-    /**
-     * Set up tests.
-     */
-    public function setUp()
-    {
-        parent::setUp();
-
-        // Set up the database
-        Artisan::call('db:seed');
-
-        // Run the tests in English
-        App::setLocale('en');
-
-        /*
-         * Create class properties to be used in tests
-         */
-        $this->admin = User::find(1);
-        $this->executive = User::find(2);
-        $this->user = User::find(3);
-        $this->adminRole = Role::find(1);
-        $this->executiveRole = Role::find(2);
-        $this->userRole = Role::find(3);
-    }
-
-    public function tearDown()
-    {
-        $this->beforeApplicationDestroyed(function () {
-            DB::disconnect();
-        });
-
-        parent::tearDown();
-    }
+    use CreatesApplication;
 }
