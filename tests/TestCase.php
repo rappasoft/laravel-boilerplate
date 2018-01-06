949051e798ff4a94f8331388a96708b098fa354b
Manuel Christlieb <manuel@christlieb.eu>
2018-01-06 02:40:26 +0100

    add TestResponse marco to follow redirects and male assertions

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ namespace Tests;
 
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\TestResponse;
 
 /**
  * Class TestCase.
@@ -11,4 +12,22 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $test = $this;
+
+        TestResponse::macro('followRedirects', function ($testCase = null) use ($test) {
+            $response = $this;
+            $testCase = $testCase ?: $test;
+
+            while ($response->isRedirect()) {
+                $response = $testCase->get($response->headers->get('Location'));
+            }
+
+            return $response;
+        });
+    }
 }
