2ad2e3065bc3b72808320d49c667ce0ad866c515
Anthony Rappa <rappa819@gmail.com>
2018-01-10 12:19:50 -0500

    - Update to laravel commit: f4cba4f2b254456645036139129142df274a1ec1

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -2,25 +2,24 @@
 
 namespace Tests;
 
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Contracts\Console\Kernel;
 
 trait CreatesApplication
 {
-    /**
-     * Creates the application.
-     *
-     * @return \Illuminate\Foundation\Application
-     */
-    public function createApplication()
-    {
-        $app = require __DIR__.'/../bootstrap/app.php';
+	/**
+	 * Creates the application.
+	 *
+	 * @return \Illuminate\Foundation\Application
+	 */
+	public function createApplication()
+	{
+		$app = require __DIR__.'/../bootstrap/app.php';
 
-        if (file_exists(dirname(__DIR__).'/.env.testing')) {
-            ( new \Dotenv\Dotenv(dirname(__DIR__), '.env.testing') )->load();
-        }
+		$app->make(Kernel::class)->bootstrap();
 
-        $app->make(Kernel::class)->bootstrap();
+		Hash::setRounds(4);
 
-        return $app;
-    }
+		return $app;
+	}
 }
