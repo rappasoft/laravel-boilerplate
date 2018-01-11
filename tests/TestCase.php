6df53d245a3329a82c02a257d385c5d9cde32a2f
Manuel Christlieb <manuel@christlieb.eu>
2018-01-11 18:20:51 +0100

    remove unused setUpAcl

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -14,51 +14,6 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 
-    /**
-     * @var
-     */
-    protected $admin;
-
-    /**
-     * @var
-     */
-    protected $executive;
-
-    /**
-     * @var
-     */
-    protected $user;
-
-    /**
-     * @var
-     */
-    protected $adminRole;
-
-    /**
-     * @var
-     */
-    protected $executiveRole;
-
-    /**
-     * @var
-     */
-    protected $userRole;
-
-    protected function setUpAcl()
-    {
-        $this->artisan('db:seed');
-
-        /*
-         * Create class properties to be used in tests
-         */
-        $this->admin = User::find(1);
-        $this->executive = User::find(2);
-        $this->user = User::find(3);
-        $this->adminRole = Role::find(1);
-        $this->executiveRole = Role::find(2);
-        $this->userRole = Role::find(3);
-    }
-
     protected function loginAsAdmin()
     {
         $adminRole = factory(Role::class)->create(['name' => config('access.users.admin_role')]);
