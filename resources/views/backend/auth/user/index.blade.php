ffcc37b00c3f6f9501fe286d562599a6d290b9e5
Lucas Martín <codedeep@users.noreply.github.com>
2018-01-11 19:57:39 +0100

    Update index.blade.php

--- resources/views/backend/auth/user/index.blade.php
+++ resources/views/backend/auth/user/index.blade.php
@@ -1,6 +1,6 @@
 @extends ('backend.layouts.app')
 
-@section ('title', 'title', app_name() . ' | ' . __('labels.backend.access.users.management'))
+@section ('title', app_name() . ' | ' . __('labels.backend.access.users.management'))
 
 @section('breadcrumb-links')
     @include('backend.auth.user.includes.breadcrumb-links')
