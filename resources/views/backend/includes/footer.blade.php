0206e0e672d28f9cbb2fc2f9e9c385683a1e64a1
Anthony Rappa <rappa819@gmail.com>
2018-03-02 10:44:41 -0500

    no message

--- resources/views/backend/includes/footer.blade.php
+++ resources/views/backend/includes/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="app-footer">
     <strong>{{ __('labels.general.copyright') }} &copy; {{ date('Y') }} <a href="http://laravel-boilerplate.com">{{ __('strings.backend.general.boilerplate_link') }}</a></strong> {{ __('strings.backend.general.all_rights_reserved') }}
 
-    <span class="pull-right">Powered by <a href="http://coreui.io">CoreUI</a></span>
+    <span class="float-right">Powered by <a href="http://coreui.io">CoreUI</a></span>
 </footer>
