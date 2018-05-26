0314c5f934198ec1d45f32e9c95e13396cc99ffa
Anthony Rappa <rappa819@gmail.com>
2018-05-26 18:31:20 -0400

    - Update breadcrumbs to new syntax

--- routes/breadcrumbs/backend/auth/role.php
+++ routes/breadcrumbs/backend/auth/role.php
@@ -1,16 +1,16 @@
 <?php
 
-Breadcrumbs::register('admin.auth.role.index', function ($breadcrumbs) {
-    $breadcrumbs->parent('admin.dashboard');
-    $breadcrumbs->push(__('menus.backend.access.roles.management'), route('admin.auth.role.index'));
+Breadcrumbs::for('admin.auth.role.index', function ($trail) {
+    $trail->parent('admin.dashboard');
+    $trail->push(__('menus.backend.access.roles.management'), route('admin.auth.role.index'));
 });
 
-Breadcrumbs::register('admin.auth.role.create', function ($breadcrumbs) {
-    $breadcrumbs->parent('admin.auth.role.index');
-    $breadcrumbs->push(__('menus.backend.access.roles.create'), route('admin.auth.role.create'));
+Breadcrumbs::for('admin.auth.role.create', function ($trail) {
+    $trail->parent('admin.auth.role.index');
+    $trail->push(__('menus.backend.access.roles.create'), route('admin.auth.role.create'));
 });
 
-Breadcrumbs::register('admin.auth.role.edit', function ($breadcrumbs, $id) {
-    $breadcrumbs->parent('admin.auth.role.index');
-    $breadcrumbs->push(__('menus.backend.access.roles.edit'), route('admin.auth.role.edit', $id));
+Breadcrumbs::for('admin.auth.role.edit', function ($trail, $id) {
+    $trail->parent('admin.auth.role.index');
+    $trail->push(__('menus.backend.access.roles.edit'), route('admin.auth.role.edit', $id));
 });
