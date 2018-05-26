15739ad35be8adc9842ce6a2befad5becc028764
Anthony Rappa <rappa819@gmail.com>
2018-05-26 19:09:54 -0400

    - Remove

--- resources/views/backend/includes/footer.blade.php
+++ resources/views/backend/includes/footer.blade.php
@@ -3,13 +3,7 @@
         <strong>{{ __('labels.general.copyright') }} &copy; {{ date('Y') }} <a href="http://laravel-boilerplate.com">{{ __('strings.backend.general.boilerplate_link') }}</a></strong> {{ __('strings.backend.general.all_rights_reserved') }}
     </span>
 
-    <span class="ml-auto">
-        Powered by <a href="http://coreui.io">CoreUI</a>
-
-        @if (config('boilerplate.show-load-time'))
-            <small>(Page generated in {{ round((microtime(true) - LARAVEL_START), 5) }} seconds)</small>
-        @endif
-    </span>
+    <span class="ml-auto">Powered by <a href="http://coreui.io">CoreUI</a></span>
 
     <div class="clearfix"></div>
 </footer>
