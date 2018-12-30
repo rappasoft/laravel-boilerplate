95100473454d1521127b6bd1aabfd664ec3fa905
Anthony Rappa <anthony@rappasoft.com>
2018-12-30 03:28:04 +0000

    Apply fixes from StyleCI

--- config/view.php
+++ config/view.php
@@ -28,9 +28,9 @@ return [
     |
     */
 
-	'compiled' => env(
-		'VIEW_COMPILED_PATH',
-		realpath(storage_path('framework/views'))
-	),
+    'compiled' => env(
+        'VIEW_COMPILED_PATH',
+        realpath(storage_path('framework/views'))
+    ),
 
 ];
