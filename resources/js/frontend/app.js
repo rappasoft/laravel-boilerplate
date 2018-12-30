498498e29796b5bbeceaf58d7ee60008134a1613
Anthony Rappa <rappa819@gmail.com>
2018-12-29 22:27:42 -0500

    - Update to laravel commit: d1db3cd089d6e9f81a4c8a65d401939f85cbbd5c

--- resources/js/frontend/app.js
+++ resources/js/frontend/app.js
@@ -19,13 +19,10 @@ window.Vue = Vue;
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
 // const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key)))
 
-// files.keys().map(key => {
-//     return Vue.component(_.last(key.split('/')).split('.')[0], files(key))
-// })
+Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
