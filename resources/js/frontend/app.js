679f1b43dd2da4090d490068bae8f25b65a88f60
Aaron Belz <belzaaron@gmail.com>
2019-01-25 04:34:14 -0500

    Added `.default` to Vue component requires
    JeffreyWay mentioned this in the "Breaking" section here: https://github.com/JeffreyWay/laravel-mix/releases/tag/v4.0.0
    with a matching commit here: https://github.com/laravel/laravel/commit/dc58f95ebbf15b8902ccaef4e2aaa6b0ecbb0e96
    to finally a PR here: https://github.com/laravel/laravel/pull/4882/commits/dc58f95ebbf15b8902ccaef4e2aaa6b0ecbb0e96#diff-b7c96b83e41797041ea5067055f8b2af
    
    Without this change upon a new clone you would get a `[Vue warn]: Failed to mount component: template or render function not defined.` error in the console.

--- resources/js/frontend/app.js
+++ resources/js/frontend/app.js
@@ -20,9 +20,9 @@ window.Vue = Vue;
  */
 
 // const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key)))
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
