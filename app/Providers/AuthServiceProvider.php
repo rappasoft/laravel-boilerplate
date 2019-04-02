75d7e64a54ecb31810542facfe2a6fdf498ae885
Bryan <bryan.scott@ffa.int>
2019-04-03 10:47:20 +1100

    Update AuthServiceProvider.php
    Include impoer

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-
+use Illuminate\Support\Facades\Gate;
 /**
  * Class AuthServiceProvider.
  */
