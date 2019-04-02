5d2632bd00e82946c4968560a5f69bc15b7394c8
Bryan <bryan.scott@ffa.int>
2019-04-03 10:49:02 +1100

    Update AuthServiceProvider.php
    StyleCI updates

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers;
 
-use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
 /**
  * Class AuthServiceProvider.
  */
