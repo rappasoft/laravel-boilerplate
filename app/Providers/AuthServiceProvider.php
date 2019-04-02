a8a14f532cf4e887ddee096018e31c411c0adaf8
Bryan <bryan.scott@ffa.int>
2019-04-03 09:47:08 +1100

    Update AuthServiceProvider
    As per spatie recommendation here : https://github.com/spatie/laravel-permission/wiki/Global-%22Admin%22-role

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,10 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        // Implicitly grant "Admin" role all permissions
+        // This works in the app by using gate-related functions like auth()->user->can() and @can()
+        Gate::before(function ($user, $ability) {
+            return $user->hasRole('Admin') ? true : null;
+        });
     }
 }
