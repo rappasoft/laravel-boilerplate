3efc277a8b1d3efc6da54bb5d9af1c0b72b6d1b4
Craig Smith <craig.smith@customd.com>
2019-05-06 14:51:42 +1200

    Json Middleware - enforcing json output

---
+++ app/Http/Middleware/JsonMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class JsonMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $request->headers->set('Accept', 'application/json');
+
+        return $next($request);
+    }
+}
