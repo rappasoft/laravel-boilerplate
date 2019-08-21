b4cf9426d9eb192ebc2da363248b3081bc66ef67
Anthony Rappa <rappa819@gmail.com>
2019-08-21 11:28:47 -0400

    - Add default

--- config/services.php
+++ config/services.php
@@ -73,6 +73,7 @@ return [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect' => env('GOOGLE_REDIRECT'),
+        'scopes' => [],
         'with' => [],
     ],
 
