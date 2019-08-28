03fe66f5374321e14853353ef0bf2be5f384b34b
Anthony Rappa <rappa819@gmail.com>
2019-08-28 09:51:35 -0400

    - Update to laravel commit: aa74fcb38f9f318159657ba5050eda62ec043b11

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -25,22 +25,9 @@ window._ = _; // Lodash
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = axios;
+window.axios = require('axios');
+
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-/**
- * Next we will register the CSRF Token as a common header with Axios so that
- * all outgoing HTTP requests automatically have it attached. This is just
- * a simple convenience so we don't have to attach every token manually.
- */
-
-const token = document.head.querySelector('meta[name="csrf-token"]');
-
-if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
@@ -54,7 +41,7 @@ if (token) {
 
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY
+//     key: process.env.MIX_PUSHER_APP_KEY,
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     encrypted: true
 // });
