e22a98b504a9bc58ea4f3af8659a7615af4c9642
Anthony Rappa <rappa819@gmail.com>
2019-09-04 14:53:11 -0400

    - Update requests

--- app/Http/Requests/Backend/Auth/Role/StoreRoleRequest.php
+++ app/Http/Requests/Backend/Auth/Role/StoreRoleRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Backend\Auth\Role;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 /**
  * Class StoreRoleRequest.
@@ -27,7 +28,7 @@ class StoreRoleRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => ['required', 'unique:roles', 'max:191'],
+            'name' => ['required', Rule::unique('roles')],
         ];
     }
 }
