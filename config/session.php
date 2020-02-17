e10305a3d445c2470f0adc835226e5b989383b7c
Anthony Rappa <rappa819@gmail.com>
2020-02-17 08:14:32 -0500

    - Update to Laravel commit: c78a1d8184f00f8270d8a135cc21590837b6bfbd

--- config/session.php
+++ config/session.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'driver' => env('SESSION_DRIVER', 'cookie'),
+    'driver' => env('SESSION_DRIVER', 'file'),
 
     /*
     |--------------------------------------------------------------------------
