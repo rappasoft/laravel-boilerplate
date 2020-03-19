9a5c16cbf7cc2bdaecba1b8f1410c07c07a71c13
skys215 <skys215@gmail.com>
2020-03-19 21:32:35 +0800

    Added datatable js

--- resources/js/backend/app.js
+++ resources/js/backend/app.js
@@ -1,1 +1,9 @@
 import '@coreui/coreui'
+
+import $ from 'jquery';
+import dt from 'datatables.net';
+import 'datatables.net-bs4';
+import 'datatables.net-buttons';
+import 'datatables.net-buttons-bs4';
+
+window.$ = window.jQuery = $;
