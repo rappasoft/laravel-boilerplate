33f1e77f5ac9bcb7660a07b4d82a56d0d5efca54
Anthony Rappa <rappa819@gmail.com>
2020-04-09 17:37:01 -0400

    Change class

--- resources/views/includes/partials/read-only.blade.php
+++ resources/views/includes/partials/read-only.blade.php
@@ -1,5 +1,5 @@
 @readonly
-    <div class="alert alert-info read-only">
+    <div class="alert alert-info alert-header">
         The Application is currently in read only mode. All requests other than GET are disabled.
     </div>
 @endreadonly
