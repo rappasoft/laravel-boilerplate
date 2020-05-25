1bf1254bdb9df95c1138d0080a517755c6196bb6
Anthony Rappa <rappa819@gmail.com>
2020-05-25 11:03:31 -0400

    Add announcement seeder with usage examples

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call(AuthTableSeeder::class);
+        $this->call(AnnouncementSeeder::class);
 
         Model::reguard();
     }
