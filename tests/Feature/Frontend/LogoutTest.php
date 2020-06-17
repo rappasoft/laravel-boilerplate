a20fded5aa7cf3f217db9a923e24eaf71ef862bb
Anthony Rappa <rappa819@gmail.com>
2020-06-16 22:34:59 -0400

    Tests WIP

---
+++ tests/Feature/Frontend/LogoutTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature\Frontend;
+
+use App\Domains\Auth\Models\User;
+use Tests\TestCase;
+
+/**
+ * Class LogoutTest.
+ */
+class LogoutTest extends TestCase
+{
+    /** @test */
+    public function the_user_can_logout()
+    {
+        $this->actingAs($user = factory(User::class)->create());
+
+        $this->assertAuthenticatedAs($user);
+
+        $this->post('/logout')
+            ->assertRedirect('/');
+
+        $this->assertFalse($this->isAuthenticated());
+    }
+}
