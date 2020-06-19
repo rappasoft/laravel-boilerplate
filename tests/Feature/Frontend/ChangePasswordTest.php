9faad15273bb2e59fd149b636497e22160e52a1b
Anthony Rappa <rappa819@gmail.com>
2020-06-19 00:11:48 -0400

    Change password history tests

--- tests/Feature/Frontend/ChangePasswordTest.php
+++ tests/Feature/Frontend/ChangePasswordTest.php
@@ -37,4 +37,89 @@ class ChangePasswordTest extends TestCase
         $response->assertSessionHas('flash_success', __('Password successfully updated.'));
         $this->assertTrue(Hash::check('OC4Nzu270N!QBVi%U%qX', $user->fresh()->password));
     }
+
+    /** @test */
+    public function a_user_can_use_the_same_password_when_history_is_off_on_account_change_password()
+    {
+        config(['boilerplate.access.user.password_history' => false]);
+
+        $user = factory(User::class)->create(['password' => 'OC4Nzu270N!QBVi%U%qX']);
+
+        $response = $this->actingAs($user)
+            ->patch('/password/update', [
+                'current_password' => 'OC4Nzu270N!QBVi%U%qX',
+                'password' => 'OC4Nzu270N!QBVi%U%qX_02',
+                'password_confirmation' => 'OC4Nzu270N!QBVi%U%qX_02',
+            ]);
+
+        $response->assertSessionHas('flash_success', __('Password successfully updated.'));
+        $this->assertTrue(Hash::check('OC4Nzu270N!QBVi%U%qX_02', $user->fresh()->password));
+    }
+
+    /** @test */
+    public function a_user_can_not_use_the_same_password_when_history_is_on_on_account_change_password()
+    {
+        config(['boilerplate.access.user.password_history' => 3]);
+
+        $user = factory(User::class)->create(['password' => 'OC4Nzu270N!QBVi%U%qX']);
+
+        // Change once
+        $this->actingAs($user)
+            ->patch('/password/update', [
+                'current_password' => 'OC4Nzu270N!QBVi%U%qX',
+                'password' => 'OC4Nzu270N!QBVi%U%qX_02',
+                'password_confirmation' => 'OC4Nzu270N!QBVi%U%qX_02',
+            ]);
+
+        $this->assertTrue(Hash::check('OC4Nzu270N!QBVi%U%qX_02', $user->fresh()->password));
+
+        // Change back
+        $response = $this->actingAs($user)
+            ->patch('/password/update', [
+                'current_password' => 'OC4Nzu270N!QBVi%U%qX_02',
+                'password' => 'OC4Nzu270N!QBVi%U%qX',
+                'password_confirmation' => 'OC4Nzu270N!QBVi%U%qX',
+            ]);
+
+        $response->assertSessionHasErrors();
+        $errors = session('errors');
+        $this->assertSame($errors->get('password')[0], __('You can not set a password that you have previously used within the last 3 times.'));
+        $this->assertTrue(Hash::check('OC4Nzu270N!QBVi%U%qX_02', $user->fresh()->password));
+    }
+
+    /** @test */
+    public function a_user_can_reuse_a_password_after_it_surpasses_the_limit()
+    {
+        config(['boilerplate.access.user.password_history' => 2]);
+
+        $user = factory(User::class)->create(['password' => 'OC4Nzu270N!QBVi%U%qX']);
+
+        // Change once
+        $this->actingAs($user)
+            ->patch('/password/update', [
+                'current_password' => 'OC4Nzu270N!QBVi%U%qX',
+                'password' => 'OC4Nzu270N!QBVi%U%qX_02',
+                'password_confirmation' => 'OC4Nzu270N!QBVi%U%qX_02',
+            ]);
+
+        $this->assertTrue(Hash::check('OC4Nzu270N!QBVi%U%qX_02', $user->fresh()->password));
+
+        // Change twice
+        $this->actingAs($user)
+            ->patch('/password/update', [
+                'current_password' => 'OC4Nzu270N!QBVi%U%qX_02',
+                'password' => 'OC4Nzu270N!QBVi%U%qX_03',
+                'password_confirmation' => 'OC4Nzu270N!QBVi%U%qX_03',
+            ]);
+
+        $response = $this->actingAs($user)
+            ->patch('/password/update', [
+                'current_password' => 'OC4Nzu270N!QBVi%U%qX_03',
+                'password' => 'OC4Nzu270N!QBVi%U%qX',
+                'password_confirmation' => 'OC4Nzu270N!QBVi%U%qX',
+            ]);
+
+        $response->assertSessionHas('flash_success', __('Password successfully updated.'));
+        $this->assertTrue(Hash::check('OC4Nzu270N!QBVi%U%qX', $user->fresh()->password));
+    }
 }
