2450b23ee72197c61ec8f8535b819363e13851fe
Anthony Rappa <rappa819@gmail.com>
2020-06-19 00:23:44 -0400

    WIP

---
+++ tests/Feature/Frontend/ResetPasswordTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature\Frontend;
+
+use Tests\TestCase;
+
+/**
+ * Class ResetPasswordTest.
+ */
+class ResetPasswordTest extends TestCase
+{
+
+    /** @test */
+    public function the_password_reset_route_exists()
+    {
+        $this->get('password/reset')->assertOk();
+    }
+}
