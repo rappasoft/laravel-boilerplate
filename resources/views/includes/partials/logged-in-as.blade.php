0ede94a1117f8824fa5374a2310af57a3058bbd4
Anthony Rappa <rappa819@gmail.com>
2020-07-04 10:23:14 -0400

    Made full screen alerts slightly shorter

--- resources/views/includes/partials/logged-in-as.blade.php
+++ resources/views/includes/partials/logged-in-as.blade.php
@@ -1,5 +1,5 @@
 @impersonating
-    <div class="alert alert-warning mb-0">
+    <div class="alert alert-warning pt-1 pb-1 mb-0">
         @lang('You are currently logged in as :name.', ['name' => $logged_in_user->name]) <a href="{{ route('impersonate.leave') }}">@lang('Return to your account')</a>.
     </div><!--alert alert-warning-->
 @endImpersonating
