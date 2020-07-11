271db9b290fef467867bc9b0caaa8daec0f1d7f7
Anthony Rappa <rappa819@gmail.com>
2020-07-11 10:46:45 -0400

    Remove excess tags

--- resources/views/frontend/user/account.blade.php
+++ resources/views/frontend/user/account.blade.php
@@ -11,75 +11,71 @@
                 </x-slot>
 
                 <x-slot name="body">
-                    <div class="row">
-                        <div class="col">
-                            <nav>
-                                <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                                    <x-utils.link
-                                        :text="__('My Profile')"
-                                        class="nav-link active"
-                                        id="my-profile-tab"
-                                        data-toggle="pill"
-                                        href="#my-profile"
-                                        role="tab"
-                                        aria-controls="my-profile"
-                                        aria-selected="true" />
+                    <nav>
+                        <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                            <x-utils.link
+                                :text="__('My Profile')"
+                                class="nav-link active"
+                                id="my-profile-tab"
+                                data-toggle="pill"
+                                href="#my-profile"
+                                role="tab"
+                                aria-controls="my-profile"
+                                aria-selected="true" />
 
-                                    <x-utils.link
-                                        :text="__('Edit Information')"
-                                        class="nav-link"
-                                        id="information-tab"
-                                        data-toggle="pill"
-                                        href="#information"
-                                        role="tab"
-                                        aria-controls="information"
-                                        aria-selected="false"/>
+                            <x-utils.link
+                                :text="__('Edit Information')"
+                                class="nav-link"
+                                id="information-tab"
+                                data-toggle="pill"
+                                href="#information"
+                                role="tab"
+                                aria-controls="information"
+                                aria-selected="false"/>
 
-                                    @if (! $logged_in_user->isSocial())
-                                        <x-utils.link
-                                            :text="__('Password')"
-                                            class="nav-link"
-                                            id="password-tab"
-                                            data-toggle="pill"
-                                            href="#password"
-                                            role="tab"
-                                            aria-controls="password"
-                                            aria-selected="false" />
-                                    @endif
+                            @if (! $logged_in_user->isSocial())
+                                <x-utils.link
+                                    :text="__('Password')"
+                                    class="nav-link"
+                                    id="password-tab"
+                                    data-toggle="pill"
+                                    href="#password"
+                                    role="tab"
+                                    aria-controls="password"
+                                    aria-selected="false" />
+                            @endif
 
-                                    <x-utils.link
-                                        :text="__('Two Factor Authentication')"
-                                        class="nav-link"
-                                        id="two-factor-authentication-tab"
-                                        data-toggle="pill"
-                                        href="#two-factor-authentication"
-                                        role="tab"
-                                        aria-controls="two-factor-authentication"
-                                        aria-selected="false"/>
-                                </div>
-                            </nav>
+                            <x-utils.link
+                                :text="__('Two Factor Authentication')"
+                                class="nav-link"
+                                id="two-factor-authentication-tab"
+                                data-toggle="pill"
+                                href="#two-factor-authentication"
+                                role="tab"
+                                aria-controls="two-factor-authentication"
+                                aria-selected="false"/>
+                        </div>
+                    </nav>
 
-                            <div class="tab-content" id="my-profile-tabsContent">
-                                <div class="tab-pane fade pt-3 show active" id="my-profile" role="tabpanel" aria-labelledby="my-profile-tab">
-                                    @include('frontend.user.account.tabs.profile')
-                                </div><!--tab-profile-->
+                    <div class="tab-content" id="my-profile-tabsContent">
+                        <div class="tab-pane fade pt-3 show active" id="my-profile" role="tabpanel" aria-labelledby="my-profile-tab">
+                            @include('frontend.user.account.tabs.profile')
+                        </div><!--tab-profile-->
 
-                                <div class="tab-pane fade pt-3" id="information" role="tabpanel" aria-labelledby="information-tab">
-                                    @include('frontend.user.account.tabs.information')
-                                </div><!--tab-information-->
+                        <div class="tab-pane fade pt-3" id="information" role="tabpanel" aria-labelledby="information-tab">
+                            @include('frontend.user.account.tabs.information')
+                        </div><!--tab-information-->
 
-                                @if (! $logged_in_user->isSocial())
-                                    <div class="tab-pane fade pt-3" id="password" role="tabpanel" aria-labelledby="password-tab">
-                                        @include('frontend.user.account.tabs.password')
-                                    </div><!--tab-password-->
-                                @endif
+                        @if (! $logged_in_user->isSocial())
+                            <div class="tab-pane fade pt-3" id="password" role="tabpanel" aria-labelledby="password-tab">
+                                @include('frontend.user.account.tabs.password')
+                            </div><!--tab-password-->
+                        @endif
 
-                                <div class="tab-pane fade pt-3" id="two-factor-authentication" role="tabpanel" aria-labelledby="two-factor-authentication-tab">
-                                    @include('frontend.user.account.tabs.two-factor-authentication')
-                                </div><!--tab-information-->
-                            </div><!--tab-content-->
-                        </div><!--col-->
-                    </div><!--row-->
+                        <div class="tab-pane fade pt-3" id="two-factor-authentication" role="tabpanel" aria-labelledby="two-factor-authentication-tab">
+                            @include('frontend.user.account.tabs.two-factor-authentication')
+                        </div><!--tab-information-->
+                    </div><!--tab-content-->
                 </x-slot>
             </x-frontend.card>
         </div><!--col-md-10-->
