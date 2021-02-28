3c5709616d029ab43a854d24e7d0ab50eeb22217
bugdetector <bakiyucel38@gmail.com>
2021-02-28 15:52:56 +0300

    announcements remove

--- app/Providers/ComposerServiceProvider.php
+++ app/Providers/ComposerServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use App\Domains\Announcement\Services\AnnouncementService;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -14,20 +13,11 @@ class ComposerServiceProvider extends ServiceProvider
     /**
      * Register bindings in the container.
      *
-     * @param  AnnouncementService  $announcementService
      */
-    public function boot(AnnouncementService $announcementService)
+    public function boot()
     {
         View::composer('*', function ($view) {
             $view->with('logged_in_user', auth()->user());
-        });
-
-        View::composer(['frontend.index', 'frontend.layouts.app'], function ($view) use ($announcementService) {
-            $view->with('announcements', $announcementService->getForFrontend());
-        });
-
-        View::composer(['backend.layouts.app'], function ($view) use ($announcementService) {
-            $view->with('announcements', $announcementService->getForBackend());
         });
     }
 }
