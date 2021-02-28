b0ca3b90a8fe2b6772de68a676d22758ef71615c
bugdetector <bakiyucel38@gmail.com>
2021-02-28 16:54:37 +0300

    Hide language select

--- resources/views/backend/includes/header.blade.php
+++ resources/views/backend/includes/header.blade.php
@@ -16,7 +16,7 @@
     <ul class="c-header-nav d-md-down-none">
         <li class="c-header-nav-item px-3"><a class="c-header-nav-link" href="{{ route('frontend.index') }}">@lang('Home')</a></li>
 
-        @if(config('boilerplate.locale.status') && count(config('boilerplate.locale.languages')) > 1)
+        {{-- @if(config('boilerplate.locale.status') && count(config('boilerplate.locale.languages')) > 1)
             <li class="c-header-nav-item dropdown">
                 <x-utils.link
                     :text="__(getLocaleName(app()->getLocale()))"
@@ -28,7 +28,7 @@
 
                 @include('includes.partials.lang')
             </li>
-        @endif
+        @endif --}}
     </ul>
 
     <ul class="c-header-nav ml-auto mr-4">
