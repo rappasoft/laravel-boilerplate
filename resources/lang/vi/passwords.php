48ce96b86f232e21e7abb6bc1e7b0bd164e2b0a9
James <tienrocker@gmail.com>
2021-05-08 14:36:32 +0700

    Add vietnamese (vi) language

---
+++ resources/lang/vi/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Mật khẩu đã được cập nhật!',
+    'sent' => 'Chúng tôi đã gửi cho bạn đường dẫn thay đổi mật khẩu!',
+    'throttled' => 'Vui lòng đợi trước khi thử lại.',
+    'token' => 'Mã xác nhận mật khẩu không hợp lệ.',
+    'user' => 'Không tìm thấy thành viên với địa chỉ email này.',
+
+];
