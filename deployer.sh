3738b841cd1f5f87851406672f2628f20ff65378
Czooi <2002czooi@gmail.com>
2024-08-08 16:25:41 +0800

    create deployer.sh

---
+++ deployer.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+echo "Deploying application ..."
+
+# Enter maintenance mode
+(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
+    # Update codebase
+    git pull origin master
+    
+# Exit maintenance mode
+php artisan up
+
+echo "Application deployed!"
